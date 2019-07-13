ce6335f87bd842375c282dd29eb7d4a1eb07b50b
Arul Selvan <Arul.Selvan@realpage.com>
2019-07-13 13:31:57 -0500

    Zap zoom webserver

---
+++ zap_zoom_ws.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# zap_zoomws.sh --- simple script to remove zooms hidden webservice from your mac
+# Author:  Arul Selvan
+# Version: Jul 13, 2019
+
+service_port=19421
+pid=`sudo lsof -n -i :$service_port | awk '{print $2;}'|uniq`
+
+if [ -z $pid ] ; then
+  echo "[INFO] Doesn't look like zoom hidden webservice is running on your host."
+  exit
+fi
+
+echo "[WARN] found zoom hidden webervice running, attempting to kill..."
+kill -9 $pid
+if [ $? -ne 0 ] ; then
+  echo "[ERROR] unable to kill the webservice. pid=$pid, update your zoom client to v4.4.53932.0709"
+else
+  echo "[INFO] killed the hidden webservice"
+fi
+
+# wipe the trace of user preference. Actually, need to touch this to be readonly.
+rm -rf ~/.zoomus/
