5d90800ce6cc1ece6d50b5cc1ce5beeb0764ada1
Arul Selvan <aselvan@selvans.net>
2020-06-26 15:37:25 -0500

    Added update function

--- docker-jitsi/jitsi_user.sh
+++ docker-jitsi/jitsi_user.sh
@@ -6,23 +6,49 @@
 # Version: May 30, 2020
 
 jitsi_host=meet.jitsi
+my_name=`basename $0`
+
+usage() {
+  echo "Usage: $my_name <create|delete|update> <username> [password]"
+  exit 0
+}
 
 create() {
   user=$1
   password=$2
+  if [[ -z $user || -z $password ]] ; then
+    echo "[ERROR] 'user' and 'password' are required for create"
+    usage
+  fi
   docker-compose exec prosody prosodyctl --config /config/prosody.cfg.lua register $user $jitsi_host $password
 }
 
 delete() {
   user=$1
+  if [ -z $user ] ; then
+    echo "[ERROR] 'user' is a required argument for delete"
+    usage
+  fi
   docker-compose exec prosody prosodyctl --config /config/prosody.cfg.lua unregister $user $jitsi_host
+}
+
+update() {
+  user=$1
+  password=$2
+  if [[ -z $user || -z $password ]] ; then
+    echo "[ERROR] 'user' and 'password' are required for update"
+    usage
+  fi
+  delete $user
+  create $user $password
 }
 
 # create or delete users
 case $1 in
-  create|delete) "$@"
+  create|delete|update) "$@"
   ;;
-  *) echo "Usage: $0 <create|delete> <username> [password]"
+  *) 
+  usage 
   ;;
 esac
 
