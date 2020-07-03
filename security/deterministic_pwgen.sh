554ef380348b15f650cfcf3ddc35402b19919291
Arul Selvan <aselvan@selvans.net>
2020-07-03 06:58:19 -0500

    Added more special chars to restricted list

--- security/deterministic_pwgen.sh
+++ security/deterministic_pwgen.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 #
 # deterministic_pwgen.sh --- simple wrapper over pwgen to produce deterministic password
 #
@@ -14,31 +13,42 @@
 # Note: Each password is specific to the username & website where these parameters are 
 # used as salt to create unique strong password along with the passphrase.
 #
+# Pre-req:
+#   Requires the pwgen utility installed. Install for your OS as shown below.
+#   macOS: brew install pwgen
+#   Linux: apt-get install pwgen
+#
+# OS: 
+#   MacOS or any Linux distro
+#
 # Author:  Arul Selvan
 # Version: Sep 2, 2019
 #
 
-# special chars to avoid (some stupid websites don't allow certain special chars)
-special_chars_to_avoid="[]\\/^}{<|>();?\""
+# special chars to avoid.
+# Note: there are some dumb/stupid websites don't allow certain special chars
+special_chars_to_avoid="*~\'\`,[]\\/^}{<|>();?\""
 
 # pwgen option
 pwgen_opt="--remove-chars=$special_chars_to_avoid -N1 -cyn -1 16"
-options="s:u:h"
+options="s:u:hp"
 website=""
 user=""
+print_console=0
+os_name=`uname -s`
+my_name=`basename $0`
+
 
 # print usage
 usage() {
   #clear
-  name=`basename $0`
   cat <<EOF
-  
-  USAGE: $name -s <website> -u <username|email>
-  
-    <website> is the one you are generating password for
-    <username|email> is used as additional info for creating salt
+Usage: $my_name -s <website> -u <username|email> [-p]
+  -s <website>        ---> The website the password is generated for.
+  -u <username|email> ---> The username used for the website specified above.
+  -p                  ---> will print the password to console.
        
-     example: $name -s yahoo.com -u foo@bar.com
+  example: $my_name -s yahoo.com -u foo@bar.com
 
 EOF
   exit
@@ -53,6 +63,9 @@ while getopts $options opt; do
     u)
       user=$OPTARG
       ;;
+    p)
+      print_console=1
+      ;;
     ?)
       usage
       ;;
@@ -63,7 +76,7 @@ while getopts $options opt; do
 done
 
 if [[ -z $website || -z $user ]]; then
-  echo "[ERROR] missing args"
+  echo "[ERROR] missing required args i.e. username or website!"
   usage
 fi
 
@@ -73,13 +86,21 @@ if [ $? -ne 0 ] ; then
   exit
 fi
 
-# echo the prompt to stderr so the output (i.e. generated password) can be captured instead of 
-# printing on shell. ex: in MacOS you can do name -s yahoo.com -u foo@bar.com|pbcopy to 
-# directly read into the past buffer.
 >&2 echo "Enter your passphrase: " 
-
 read -s pass_phrase
-
 passwd=`pwgen $pwgen_opt -H <(printf $pass_phrase)#$user:$website`
 
-echo $passwd
+# copy to pastebuffer for easy access
+# pastebuffer depending on OS
+if [ $os_name = "Darwin" ]; then
+  echo -n $passwd | pbcopy
+else
+  echo -n $passwd | xsel --clipboard --input
+fi
+echo "[INFO] generated password is now copied into your paste buffer for easy access."
+
+# print to console if requested
+if [ $print_console -eq 1 ] ; then
+  echo "[INFO] your generated password is: $passwd"
+fi
+
