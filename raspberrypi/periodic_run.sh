5afac2d83564e460bafd952b23c77373be4a6a1f
Arul Selvan <aselvan@selvans.net>
2020-07-04 12:12:01 -0500

    crontab and associated scripts, not tested

---
+++ raspberrypi/periodic_run.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#
+# periodic_run.sh 
+#
+# This script is setup as cronjob to run every 5 minutes. It should try and 
+# complete under 5 minutes and if it takes longer, the cron interval should 
+# be adjusted; but cronjob is protected from mulitple invocation anyways.
+#
+# Note: copy this script to /root/scripts or other place refered in crontab path
+#
+# Author:  Arul Selvan
+# Version: Jul 4, 202
+#
+
+# google dns for validating connectivity
+gdns=8.8.8.8
+my_name=`basename $0`
+log_file="/tmp/$my_name.log"
+home_public_ip=`dig +short selvans.net`
+
+pi_is_home() {
+  echo "[INFO] PI is home!" >> $log_file
+  
+  # TODO: copy the tesla dashcam data here... (need to figure out how often we do this)
+
+  # do any other stuff we want here.
+}
+
+pi_is_not_home() {
+  
+  echo "[INFO] PI somewhere other than home!" >> $log_file
+  
+  # TODO: do any other stuff we want here.
+}
+
+#  ---------- main ---------------
+echo "[INFO] `date`: $my_name starting..." > $log_file
+
+# first check if we got connectivity.
+/sbin/ping -t30 -c3 -qo $gdns >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  echo "[WARN] We dont have connectivity. force dhcpd? or force wpa_supplicant to try again?" >> $log_file
+  # for now, exit; will figureout how to force wlan to reconnect.
+  exit
+fi
+
+# we got connectivity, see if we are at home (NOTE: reads the router IP from env variable)
+if [ -z "${HOME_PUBLIC_IP}" ] ; then
+  echo "[INFO] HOME_PUBLIC_IP not set, assuming this is us (selvans.net) " >> $log_file
+else
+  home_public_ip=${HOME_PUBLIC_IP}
+fi
+
+# find pi's egress IP
+my_ip=`/usr/bin/dig -p443 +short myip.opendns.com @resolver1.opendns.com`
+echo "[INFO] `hostname` public IP is: $my_ip" >> $log_file
+
+if [ ${home_public_ip} = ${my_ip} ] ; then
+  pi_is_home
+else
+  pi_is_not_home
+fi
+
+# TODO: more stuff later ...
+
+
---
+++ raspberrypi/poweron_run.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# poweron_run.sh 
+#
+# Power on script i.e. when tesla wakes up the hub is powered so our PI 
+# should be booting. This script make use of this to do anthying we want
+# to do when PI is powered on
+#
+# Note: copy this script to /root/scripts or other place refered in crontab path
+#
+# Author:  Arul Selvan
+# Version: Jul 4, 202
+
+# google dns for validating connectivity
+gdns=8.8.8.8
+my_name=`basename $0`
+log_file="/tmp/$my_name.log"
+home_public_ip=`dig +short selvans.net`
+# should take this also as env option later.
+ifttt_event_name="pizero"
+ifttt_api="https://maker.ifttt.com/trigger/$ifttt_event_name/with/key"
+
+#  ---------- main ---------------
+echo "[INFO] `date`: $my_name starting..." > $log_file
+
+# first check if we got connectivity.
+/sbin/ping -t30 -c3 -qo $gdns >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  echo "[WARN] We dont have connectivity. force dhcpd? or force wpa_supplicant to try again?" >> $log_file
+  # for now, exit; will figureout how to force wlan to reconnect.
+  exit
+fi
+
+# check if we have IFTTT creds are provided via env variable
+if [ -z "${IFTTT_KEY}" ] ; then
+  echo "[ERROR] no IFTTT_KEY no message is sent via IFTTT!" >> $log_file
+  exit
+fi
+
+# find pi's egress IP
+my_ip=`/usr/bin/dig -p443 +short myip.opendns.com @resolver1.opendns.com`
+pi_hostname=`hostname`
+timestamp=`date`
+
+# post a message to the IFTTT
+ifttt_endpoint="$ifttt_api/$IFTTT_KEY"
+curl -X POST \
+  -F "value1=$my_ip" \
+  -F "value2=$pi_hostname rebooted at $timestamp" \
+  $ifttt_endpoint
+
+# TODO: more stuff later ...
+
