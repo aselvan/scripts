fff0e6976863626eb96b1ec96f3d2577ba18c55a
Arul Selvan <aselvan@selvans.net>
2020-08-18 12:21:03 -0500

    fixed tee to actually append to log

--- java/jvm_oldgen.sh
+++ java/jvm_oldgen.sh
@@ -74,7 +74,7 @@ check_tools() {
 }
 
 terminate() {
-  echo "[INFO] sending term signal " || tee -a $log_file
+  echo "[INFO] sending term signal " | tee -a $log_file
   kill -15 $pid
   # give it some time 
   sleep $sleep_seconds
@@ -82,32 +82,32 @@ terminate() {
   kill -0 $pid >/dev/null 2>&1
   if [ $? -eq 0 ] ; then
     # not going down gracefully, kill it
-    echo "[INFO] not going down gracefully sending kill signal " || tee -a $log_file    
+    echo "[INFO] not going down gracefully sending kill signal " | tee -a $log_file    
     kill -9 $pid
   fi
 }
 
 restart_app() {
-  echo "[INFO] restarting ... " || tee -a $log_file
-  echo "[INFO] executing $stop_command  " || tee -a $log_file
+  echo "[INFO] restarting ... " | tee -a $log_file
+  echo "[INFO] executing $stop_command  " | tee -a $log_file
   $stop_command >> $log_file 2>&1
-  echo "[INFO] sleeping $sleep_seconds sec to start the app..." || tee -a $log_file
+  echo "[INFO] sleeping $sleep_seconds sec to start the app..." | tee -a $log_file
   sleep $sleep_seconds
-  echo "[INFO] executing $start_command  " || tee -a $log_file
+  echo "[INFO] executing $start_command  " | tee -a $log_file
   $start_command >> $log_file 2>&1
-  echo "[INFO] wait for sometime for app to startup." || tee -a $log_file
+  echo "[INFO] wait for sometime for app to startup." | tee -a $log_file
 }
 
 take_action() {
-  echo "[WARN] oldgen is larger than threshold of $oldgen_threshold%, taking action!" || tee -a $log_file
+  echo "[WARN] oldgen is larger than threshold of $oldgen_threshold%, taking action!" | tee -a $log_file
 
   if [ $terminate -ne 0 ] ; then
     terminate
   elif [ $restart_app -ne 0 ] ; then
     restart_app
   else
-    echo "[INFO] forcing a full GC" || tee -a $log_file
-    ${JDK_HOME}/bin/jcmd $pid GC.run || tee -a $log_file 2>&1
+    echo "[INFO] forcing a full GC" | tee -a $log_file
+    ${JDK_HOME}/bin/jcmd $pid GC.run | tee -a $log_file 2>&1
   fi
 }
 
@@ -144,13 +144,13 @@ check_tools
 
 # check pid
 if [ $pid -eq 0 ] ; then
-  echo "[WARN] JVM pid is not provided, will use running tomcat (assuming just only one is running)" || tee -a $log_file
+  echo "[WARN] JVM pid is not provided, will use running tomcat (assuming just only one is running)" | tee -a $log_file
   pid=`${JDK_HOME}/bin/jcmd -l|grep org.apache.catalina.startup.Bootstrap |awk '{print $1;}'`
-  echo "[INFO] found a tomcat pid $pid ... " || tee -a $log_file
+  echo "[INFO] found a tomcat pid $pid ... " | tee -a $log_file
 fi
 
 if [ ! -d /proc/$pid ] ; then
-  echo "[ERROR] $pid is invalid!" || tee -a $log_file
+  echo "[ERROR] $pid is invalid!" | tee -a $log_file
   usage
 fi
 
@@ -160,16 +160,16 @@ oldgen_cur=${result[5]}
 num_full_gc=${result[7]}
 oldgen_used_percent=$(printf %.0f $(echo "scale=2; ($oldgen_cur/$oldgen_max)*100"|bc))
 
-echo "[INFO] Oldgen MAX size: $oldgen_max KB" || tee -a $log_file
-echo "[INFO] Oldgen CUR size: $oldgen_cur KB" || tee -a $log_file
-echo "[INFO] Oldgen Used:     $oldgen_used_percent%" || tee -a $log_file
-echo "[INFO] Total full GC:   $num_full_gc times" || tee -a $log_file
+echo "[INFO] Oldgen MAX size: $oldgen_max KB" | tee -a $log_file
+echo "[INFO] Oldgen CUR size: $oldgen_cur KB" | tee -a $log_file
+echo "[INFO] Oldgen Used:     $oldgen_used_percent%" | tee -a $log_file
+echo "[INFO] Total full GC:   $num_full_gc times" | tee -a $log_file
 
 # generate thread dump only if oldgen_used_percent greater than threaddump_threshold
 if [ $oldgen_used_percent -ge $threaddump_threshold ]  ; then
-  echo "[INFO] dumping threads..." || tee -a $log_file
+  echo "[INFO] dumping threads..." | tee -a $log_file
   ${JDK_HOME}/bin/jstack -l $pid > /tmp/jvm_stackdump_$pid.txt 2>&1
-  echo "[INFO] stack dump for pid $pid is at /tmp/jvm_stackdump_$pid.txt" || tee -a $log_file
+  echo "[INFO] stack dump for pid $pid is at /tmp/jvm_stackdump_$pid.txt" | tee -a $log_file
 fi
 
 # check to see if we need to take action
