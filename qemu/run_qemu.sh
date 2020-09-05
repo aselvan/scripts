81155dff57e5e042dc91e9ea49b647b26b0a8b14
Arul Selvan <aselvan@selvans.net>
2020-09-05 08:11:31 -0500

    added image option

--- qemu/run_qemu.sh
+++ qemu/run_qemu.sh
@@ -12,10 +12,11 @@ machine=pc-q35-2.10
 # net example to forward ports
 #net="user,hostfwd=tcp:127.0.0.1:3389-:3389"
 net="user"
-boot_image="/Users/arul/VirtualBoxVMs/qemu/W7-rp.qcow2"
+image_path="$HOME//VirtualBoxVMs/qemu"
+boot_image="$image_path/winblows.qcow2"
 cpu="IvyBridge"
 vga=virtio
-options_list="m:c:wh"
+options_list="m:c:i:wh"
 my_name=`basename $0`
 log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
 
@@ -25,9 +26,10 @@ log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
 #-audiodev coreaudio,id=coreaudio
 
 usage() {
-  echo "Usage: $my_name [-m <memory> -c <count> -w -h]"
+  echo "Usage: $my_name [-m <memory> -c <count> -i <image> -w -h]"
   echo "  -m <memory> size in MB to reserve for qemu. default is 4096"
   echo "  -c <count> count of cpu core to reserve for qemu. default is 2"
+  echo "  -i <image> image name from $image_path ex: -i linux"
   echo "  -w enable writing to base image. default is snapshot mode"
   echo "  -h usage/help"
   exit
@@ -44,6 +46,9 @@ while getopts "$options_list" opt; do
     c)
       cores=$OPTARG
       ;;
+    i)
+      boot_image="$image_path/${OPTARG}.qcow2"
+      ;;
     w)
       snapshot=""
       ;;
@@ -56,7 +61,14 @@ while getopts "$options_list" opt; do
    esac
 done
 
+# ensure image exists
+if [ ! -f $boot_image ]; then 
+  echo "[ERROR] boot image ($boot_image) does not exist!"
+  usage
+fi
+
 echo "[INFO] options: memory=$memory; cpu=$cores; snapshot=$snapshot ..." >> $log_file
+echo "[INFO] options: image=$boot_image ..." >> $log_file
 
 exec qemu-system-x86_64 \
   -rtc base=localtime,clock=host \
