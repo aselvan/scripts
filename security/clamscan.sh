dadf478c22bd748d4f03dd1c5f6c0c4da84c6b6b
Arul Selvan <aselvan@selvans.net>
2020-12-27 10:03:04 -0600

    Added update only option and misl changes

--- security/clamscan.sh
+++ security/clamscan.sh
@@ -21,7 +21,7 @@ macos_unreadable="com.apple.homed.notbackedup.plist|com.apple.homed.plist|com.ap
 exclude_files=".swf|.ova|.vmdk|.mp3|.mp4|.jpg|.jpeg|.JPG|.MTS|.jar|.pst|.ost|.mov|.pack|$macos_unreadable"
 
 # other variables don't need to be changed
-options_list="hvcp:m:f:l:d:"
+options_list="uhvcp:m:f:l:d:"
 my_name=`basename $0`
 log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
 virus_report=/tmp/virus_report.log
@@ -43,6 +43,7 @@ freshclam_bin="$clamscan_path_mac/freshclam"
 sha256sum_bin="/usr/local/bin/sha256sum"
 single_file=""
 clamav_lib_path=""
+update_signature_only=0
 # mail variables
 subject="ClamAv virus scan report [Host: $my_host]"
 mail_to=""
@@ -51,22 +52,25 @@ verbose_opt="--quiet"
 clamscan_opts="-r -i -o --max-filesize=$max_file_size --detect-pua=yes --log=$virus_report --exclude-dir=$exclude_dirs --exclude=$exclude_files --bytecode-unsigned --bytecode-timeout=120000"
 
 usage() {
-  echo "Usage: $my_name [-v] [-f <file>] [-c] [-m <email_address>] [-l <log_file_path>] [-p <paths_to_scan>] [-d <days>] -v"
+  echo "Usage: $my_name [options]"
   echo "    -p <paths_to_scan> list of directories to scan in quotes. note: the default is '/'"
   echo "    -c scan only changed files since the last $days_since days"
   echo "    -d <days> number of days to check for changed files. default: $days_since days"
   echo "    -f <single_file> scan a single file and exit"
   echo "    -m <email_address> enable email and send scan results"
   echo "    -l <log_file_path> log file path, default=$log_file"
+  echo "    -u update signature only (i.e. freshclam, urlhouse filter, pua setup etc) don't scan"
   echo "    -v enable verbose mode"
   exit
 }
 
 scan_single_file() {
-  echo "scanning file: '$single_file' ... " | tee -a $log_file
-  if [ -f $single_file ]; then
-    $clamscan_bin $verbose_opt $clamscan_opts $single_file | tee -a $log_file
+  echo "[INFO] scanning file: '$single_file' ... " | tee -a $log_file
+  if [ -f "$single_file" ]; then
+    $clamscan_bin $verbose_opt $clamscan_opts $single_file >> $log_file
     exit_code=$?
+    echo "[INFO] scan results..." | tee -a $log_file
+    cat $virus_report
   else
     echo "[ERROR] file '$single_file' does not exist!" | tee -a $log_file
   fi
@@ -98,6 +102,7 @@ get_urlhaus_sig() {
   curl -s $urlhaus_sig_url -o $urlhaus_sig_file
   if [ $? -ne 0 ]; then
     echo "[ERROR] failed to download urlhaus signature file '$urlhaus_sig_file'" >> $log_file
+    exit_code=11
     return
   fi
 
@@ -105,6 +110,7 @@ get_urlhaus_sig() {
   echo "`curl -s $urlhaus_sig_md5_url` $urlhaus_sig_file" | $sha256sum_bin -c >> $log_file 2>&1
   if [ $? -ne 0 ] ; then
     echo "[ERROR] MD5 sum does not match for '$urlhaus_sig_file', skiping urlhaus signature..." >> $log_file
+    exit_code=12
     return
   fi
   
@@ -112,6 +118,7 @@ get_urlhaus_sig() {
   $clamscan_bin $urlhaus_sig_file >/dev/null 2>&1
   if [ $? -ne 0 ] ; then
     echo "[ERROR] scan failed for $urlhaus_sig_file, so ignoring the file" >> $log_file
+    exit_code=13
     return
   fi
  
@@ -121,7 +128,10 @@ get_urlhaus_sig() {
 }
 
 setup_pua() {
+  #
   # ensure the PUA override file is there (it will be gone when clamscan is updated, so always write one)
+  # CAUTION: ignoring these are not good but too much of noise/false positives forced me to add these!
+  #
   echo "[INFO] Setting up PUA over-ride entries ..." >> $log_file
   echo "[INFO] clamav HOME=$clamav_lib_path" >> $log_file
   echo "[INFO] creating clamav overide file ($clamav_lib_path/local.ign2) ..." >> $log_file
@@ -151,8 +161,42 @@ EOF
   cat $clamav_lib_path/local.ign2 >> $log_file
 }
 
-# --------------- main ----------------------
-# parse commandline
+update_all() {
+  # do a freshclam first
+  echo "[INFO] Get latest virus signatures..." >> $log_file
+  if [ -f $freshclam_log_file ] ; then
+    rm -f $freshclam_log_file
+  fi
+
+  # print current version
+  echo "[INFO] current sig version: `$freshclam_bin -V`" >> $log_file
+
+  # update the sigs
+  $freshclam_bin -l $freshclam_log_file >> $log_file 2>&1
+  exit_code=$?
+
+  # print the updated version
+  echo "[INFO] updated sig version: `$freshclam_bin -V`" >> $log_file
+
+  # update urlhaus signature
+  get_urlhaus_sig
+
+  # setup the PUA overide
+  setup_pua
+}
+
+# ---------------------------- main --------------------------------
+
+# first, clamav path for target OS, write log header etc.
+get_clamav_path
+echo "VIRUS SCAN log" > $log_file
+echo "---------------" >> $log_file
+echo "" >> $log_file
+if [ -f  $virus_report ]; then
+  rm -f $virus_report
+fi
+
+# now, parse commandline
 while getopts "$options_list" opt ; do
   case $opt in 
     p)
@@ -177,21 +221,17 @@ while getopts "$options_list" opt ; do
     d)
       days_since=$OPTARG
       ;;
+    u)
+      update_all
+      exit $exit_code
+      ;;
     h)
       usage
       ;;
   esac
 done
 
-# get all clamav path
-get_clamav_path
 
-echo "VIRUS SCAN log" > $log_file
-echo "---------------" >> $log_file
-echo "" >> $log_file
-if [ -f  $virus_report ]; then
-  rm -f $virus_report
-fi
 echo "[INFO] Scan start:   `date`" >> $log_file
 echo "[INFO] Scan host:    $my_host" >> $log_file
 if [ ! -z $mail_to ] ; then
@@ -202,35 +242,16 @@ echo "[INFO] Scan bin:     $clamscan_bin " >> $log_file
 echo "[INFO] Scan lib:     $clamav_lib_path " >> $log_file
 echo "[INFO] Scan options: $verbose_opt $clamscan_opts " >> $log_file
 
-
-# only if this is for a single scan
-if [ ! -z $single_file ] ; then
+# special case: single file scan; need to scan fast, so not updating signature
+if [ ! -z "$single_file" ] ; then
   scan_single_file
   exit $exit_code
 fi
 
-# do a full or partial scan as needed
-if [ $changed_only -eq 1 ]; then
-  echo "[INFO] Scanning only changed files in the last $days_since days" >> $log_file
-else
-  echo "[INFO] Scanning all files ..." >> $log_file
-fi
-echo "" >> $log_file
+# update everything, sig database (freshclam), pua, and urlhaus filter etc.
+update_all
 
-# do a freshclam first
-echo "[INFO] Get latest virus signatures..." >> $log_file
-if [ -f $freshclam_log_file ] ; then
-  rm -f $freshclam_log_file
-fi
-$freshclam_bin -l $freshclam_log_file >> $log_file 2>&1
-
-# update urlhaus signature
-get_urlhaus_sig
-
-# setup the PUA overide
-setup_pua
-
-# run the scan
+# finally, run the scan
 if [ $changed_only -eq 1 ] ; then
   if [ -f $changed_files ] ; then
     rm -f $changed_files
@@ -248,6 +269,7 @@ else
   $clamscan_bin $verbose_opt $clamscan_opts $scan_path >> $log_file 2>&1
   exit_code=$?
 fi
+echo "" >> $log_file
 
 echo "[INFO] Scan end for $scan_path: `date`" >> $log_file
 echo "[INFO] return code: $exit_code" >> $log_file
