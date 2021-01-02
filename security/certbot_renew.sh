e9c24567c93305408074d7fc67865d350845fd06
Arul Selvan <aselvan@selvans.net>
2021-01-02 09:25:09 -0600

    changed to ask confirmation for each domain so you can skip as needed

--- security/certbot_renew.sh
+++ security/certbot_renew.sh
@@ -33,15 +33,6 @@ list_certs() {
   exit
 }
 
-renew_certs() {
-  for d in $domain_list ; do
-    echo "[INFO] renewing certs for domanin: $d" | tee -a $log_file
-    echo "[INFO] update DNS records on $d when prompted and hit enter to continue..." | tee -a $log_file
-    echo "[INFO] ### NOTE: it may take upto 30min for DNS record to update, so give plenty of time beore continuiing!" | tee -a $log_file
-    $certbot_bin certonly --manual -d *.$d -d $d $certbot_args --email $my_email 2>&1 | tee -a $log_file
-  done
-}
-
 
 echo "[INFO] `date`: $my_name log ..." > $log_file
 # --------------- main ----------------------
@@ -66,12 +57,26 @@ if [ -z $my_email ] ; then
   usage
 fi
 
-# just confrim before renewal
-echo "[INFO] about to renew the following domains: $domain_list" | tee -a $log_file
-read -p "Are you sure? (y/n) " -n 1 -r
-echo 
-if [[ $REPLY =~ ^[Yy]$ ]] ; then
-  renew_certs
-else
-  echo "[INFO] renew cancelled" | tee -a $log_file
-fi
+# some documentation
+echo "-------------------------- README --------------------------"
+echo "Each domain renewal requres 2 TXT records. For some reason, "
+echo "the first record is pretty quick but the second one takes   "
+echo "a while to be available, so wait at aleast 30 min to ensure "
+echo "the second TXT record shows up in DNS query                 "
+echo "------------------------------------------------------------"
+echo
+
+# confrim before renewal of each domain
+for d in $domain_list ; do
+  echo "[INFO] ########## About to renew domain: '$d' ########## " | tee -a $log_file
+  read -p "Are you sure? (y/n) " -n 1 -r
+  echo 
+  if [[ $REPLY =~ ^[Yy]$ ]] ; then
+    echo "[INFO] renewing certs for domanin: $d" | tee -a $log_file
+    echo "[INFO] update DNS records on $d when prompted and hit enter to continue..." | tee -a $log_file
+    echo "[INFO] ### NOTE: it may take upto 30min for DNS record to update, so give plenty of time beore continuiing!" | tee -a $log_file
+    $certbot_bin certonly --manual -d *.$d -d $d $certbot_args --email $my_email 2>&1 | tee -a $log_file
+  else
+    echo "[INFO] skiping renewal of domain '$d'" | tee -a $log_file
+  fi
+done
