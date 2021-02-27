e5797735edde12b460a6f8ff68f3db6103f6f415
Arul Selvan <aselvan@selvans.net>
2021-02-27 11:33:49 -0600

    added quite option

--- security/secret.sh
+++ security/secret.sh
@@ -13,9 +13,11 @@
 
 os_name=`uname -s`
 my_name=`basename $0`
-log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
 options="s:vqh"
 
+info=1
+operation=""
+log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
 encFileBaseName="kanakku.txt"
 search_string=""
 gpg_opt="-qd"
@@ -29,6 +31,7 @@ Usage: $my_name [options]
   
   -s <string> ==> decrypt and regex search for 'string' in the content
   -v          ==> decrypt and open the file w/ vi (view)
+  -q          ==> no information output to console.
 
   example: $my_name -s foobar
 
@@ -36,24 +39,33 @@ EOF
   exit
 }
 
+log() {
+  local msg=$1
+
+  if [ $info -eq 0 ] ; then
+    return
+  fi
+  echo $msg |tee -a $log_file
+}
+
 # validate path, file etc.
 do_check() {
   # directory where the encrypted files are stored
-  echo "[INFO] checking KEYS_HOME in environment ..." | tee -a $log_file
+  log "[INFO] checking KEYS_HOME in environment ..."
   if [ -z $KEYS_HOME ] ; then
     KEYS_HOME="$HOME/data/personal/keys"
-    echo "[INFO] KEYS_HOME not found, defaulting to $KEYS_HOME" |tee -a $log_file
+    log "[INFO] KEYS_HOME not found, defaulting to $KEYS_HOME"
   else
-    echo "[INFO] KEYS_HOME is $KEYS_HOME" |tee -a $log_file  
+    log "[INFO] KEYS_HOME is $KEYS_HOME"
   fi
 
   if [ ! -d $KEYS_HOME ] ; then
-    echo "[ERROR] KEYS_HOME=$KEYS_HOME does not exists!" | tee -a $log_file
+    log "[ERROR] KEYS_HOME=$KEYS_HOME does not exists!"
     exit 1
   fi
   cd $KEYS_HOME || exit 2
   if [[ ! -f $encFileBaseName.enc || ! -f $encFileBaseName.gpg ]] ; then
-    echo "[ERROR] missing encrypted file(s) [$encFileBaseName.enc or $encFileBaseName.gpg]" | tee -a $log_file
+    log "[ERROR] missing encrypted file(s) [$encFileBaseName.enc or $encFileBaseName.gpg]"
     usage
   fi
   # reset vi_bin path on macOS
@@ -65,10 +77,10 @@ do_check() {
 do_view() {
   # prefere GPG if exists
   if [ -f $encFileBaseName.gpg ] ; then
-    echo "[INFO] decrypting $encFileBaseName.gpg for view ..." | tee -a $log_file 
+    log "[INFO] decrypting $encFileBaseName.gpg for view ..."
     gpg $gpg_opt $encFileBaseName.gpg | $vi_bin -
   else
-    echo "[INFO] decrypting $encFileBaseName.enc for view ..." | tee -a $log_file 
+    log "[INFO] decrypting $encFileBaseName.enc for view ..."
     openssl $openssl_opt | $vi_bin -
   fi
   exit
@@ -76,35 +88,36 @@ do_view() {
 
 do_search() {
   if [ -z $search_string ] ; then
-    echo "[ERROR] search string is missing!" | tee -a $log_file
+    log "[ERROR] search string is missing!"
     usage
   fi
   # prefere GPG if exists
   if [ -f $encFileBaseName.gpg ] ; then
-    echo "[INFO] decrypting $encFileBaseName.gpg for search ..." | tee -a $log_file
+    log "[INFO] decrypting $encFileBaseName.gpg for search ..."
     echo
     gpg $gpg_opt $encFileBaseName.gpg | egrep -i "$search_string"
   else
     echo
-    echo "[INFO] decrypting $encFileBaseName.enc for search ..." | tee -a $log_file 
+    log "[INFO] decrypting $encFileBaseName.enc for search ..."
     openssl $openssl_opt $encFileBaseName.enc | egrep -i "$search_string"
   fi
   exit
 }
 
 # ---------------- main entry --------------------
-echo "[INFO] `date`: $my_name starting ..." | tee $log_file
-do_check
 
 # commandline parse
 while getopts $options opt; do
   case $opt in
     s)
       search_string=$OPTARG
-      do_search
+      operation="search"
       ;;
     v)
-      do_view
+      operation="view"
+      ;;
+    q)
+      info=0
       ;;
     ?)
       usage
@@ -115,5 +128,17 @@ while getopts $options opt; do
     esac
 done
 
-# if no args show usage
-usage
+echo "" > $log_file
+log "[INFO] `date`: $my_name starting ..."
+do_check
+
+if [ -z $operation ] ; then
+  log "[INFO] missing arguments!"
+  usage
+fi
+
+if [ $operation = "view" ] ; then
+  do_view
+else
+  do_search
+fi
