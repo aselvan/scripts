f064a0f5b751200d5196190427b46c2ea2831b68
Arul Selvan <aselvan@selvans.net>
2021-03-06 20:30:10 -0600

    Hacked hangup to work

--- tools/obi.sh
+++ tools/obi.sh
@@ -2,6 +2,8 @@
 #
 # obi.sh --- wrapper script to interact w/ OBi200 (or any OBi device)
 #
+# pre_req:
+#   brew install html-xml-utils
 #
 # Author:  Arul Selvan
 # Version: Dec 6, 2014 
@@ -17,12 +19,6 @@ audio_path="$HOME/obi-recordings"
 obienv_file="$HOME/.obienv"
 # default port
 port=0
-
-# TODO
-# The following ID is needed to hangup the call so make sure this matches your device. This value is
-# available on response of GET call to callstatus.html but I am too lazy to do that as it doesn't 
-# appear to change for me.
-call_status_item=0x47d158
 
 usage() {
   cat <<EOF
@@ -73,9 +69,14 @@ do_reboot() {
 }
 
 do_hangup() {
-  local url="http://${OBI_HOST}/callstatus.htm?item=$call_status_item"  
+  local url="http://${OBI_HOST}/callstatus.htm"
   echo "[INFO] attempting to hangup the active call on OBi device ..." | tee -a $log_file
   wget --user="${OBI_USER}" --password="${OBI_PASSWORD}" -q -O $obi_output $url 2>&1 | tee -a $log_file
+  item_id=`hxnormalize $obi_output | grep item|awk -F= '{print $3}'|tr -d \"`
+  wget --user="${OBI_USER}" --password="${OBI_PASSWORD}" -q -O $obi_output "${url}?item=$item_id"  2>&1 | tee -a $log_file
+  # for some reason OBi rings again if the caller continues to hold, so sleep few secs and do it again
+  sleep 18
+  wget --user="${OBI_USER}" --password="${OBI_PASSWORD}" -q -O $obi_output "${url}?item=$item_id"  2>&1 | tee -a $log_file
   cat $obi_output >> $log_file
   exit 0
 }
