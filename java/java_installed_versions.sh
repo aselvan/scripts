97888b532195034918ae8c9920a976d67bbd19e1
Arul Selvan <Arul.Selvan@realpage.com>
2021-06-01 16:37:44 -0500

    check multiple locations

--- java/java_installed_versions.sh
+++ java/java_installed_versions.sh
@@ -9,25 +9,32 @@
 
 my_name=`basename $0`
 log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
-std_java_location="/usr/lib/jvm/"
-
-if [ ! -d $std_java_location ]; then
-  echo "[INFO] `hostname` doesn't appear to have java installed ... exiting"
-  exit 1
-fi
-
-echo "[INFO] checking java versions on `hostname` ..." | tee $log_file
-
 # NOTE: This is not a fool proof way as someone could have installed java anywhere like /opt/myjava
-java_dirs=`find $std_java_location -maxdepth 1 -type d |awk 'NR >1 {print $1; }'`
+std_java_locations="/usr/lib/jvm/ /usr/java"
 
-for jdir in $java_dirs ; do
-  if [ -x $jdir/bin/java ] ; then
-    java_vendor=`$jdir/bin/java -version 2>&1 | awk '/Runtime/ {print $1;}'`
-  elif [ -x $jdir/jre/bin/java ] ; then
-    java_vendor=`$jdir/jre/bin/java -version 2>&1 | awk '/Runtime/ {print $1;}'`
-  else
-    java_vendor="NOT Java Installation"
+check_java() {
+  location=$1
+  java_dirs=`find $location -maxdepth 1 -type d |awk 'NR >1 {print $1; }'`
+  if [ -z "$java_dirs" ]; then
+    echo "[INFO] no java found at '$location' "
+    continue
   fi
-  echo -e "$jdir \t| $java_vendor" | tee -a $log_file
+
+  for jdir in $java_dirs ; do
+    if [ -x $jdir/bin/java ] ; then
+      java_vendor=`$jdir/bin/java -version 2>&1 | awk '/Runtime/ {print $1;}'`
+    elif [ -x $jdir/jre/bin/java ] ; then
+      java_vendor=`$jdir/jre/bin/java -version 2>&1 | awk '/Runtime/ {print $1;}'`
+    else
+      java_vendor="NOT Java Installation"
+    fi
+    echo -e "\t$jdir \t| $java_vendor" | tee -a $log_file
+  done
+}
+
+echo "[INFO] checking java @`hostname` ..."
+for loc in $std_java_locations ; do
+  if [ -d $loc ]; then
+    check_java $loc
+  fi
 done
