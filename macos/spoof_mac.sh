87297933bd052336669ef5faff92ea2b0ba468d0
Arul Selvan <aselvan@selvans.net>
2021-07-31 06:57:57 -0500

    Reworked to for changes in macos

--- macos/spoof_mac.sh
+++ macos/spoof_mac.sh
@@ -3,7 +3,7 @@
 # spoof_mac.sh
 #
 # Description: 
-#  Spoof mac address, save current or restore.
+#  Spoof mac address, save current or restore or list.
 # 
 # pre-req: openssl
 #
@@ -19,15 +19,29 @@
 
 my_mac_addr_file="$HOME/.my_mac_address"
 iface="en0"
-options_list="i:m:lsrh"
+options_list="i:m:a:lsrh"
 airport_bin="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
-wifi_access_point="AA-Inflight"
-
+ap_ssid="AA-Inflight"
+mac_to_spoof=""
+operation=0 # [1=spoof, 2=save, 3=restore, 4=list]
 
 check_root() {
   if [ `id -u` -ne 0 ] ; then
     echo "[ERROR] you must be 'root' to run this script... exiting."
     exit
+  fi
+}
+
+save_mac() {
+  echo "[INFO] saving my mac address ..."
+  if [ ! -f $my_mac_addr_file ] ; then
+    echo "[WARN] no previously saved mac file ($my_mac_addr_file) found!"
+    echo "[WARN] saving current mac address as saved mac address ..."
+    my_mac=`ifconfig $iface|grep ether|awk '{print $2;}'`
+    echo $my_mac > $my_mac_addr_file
+  else
+    my_mac=`cat $my_mac_addr_file`
+    echo "[INFO] my mac address $my_mac is saved for future restore."
   fi
 }
 
@@ -40,64 +54,64 @@ restore_mac() {
   echo "[INFO] restoring mac to $my_mac on interface '$iface' ..."
   $airport_bin $iface -z
   ifconfig $iface ether $my_mac
-  networksetup -setairportnetwork $iface $wifi_access_point
+  networksetup -setairportnetwork $iface $ap_ssid
   ifconfig $iface down
   ifconfig $iface up
-  exit
-}
-
-save_mac() {
-  echo "[INFO] saving my mac address ..."
-  my_mac=`ifconfig $iface|grep ether|awk '{print $2;}'`
-  echo $my_mac > $my_mac_addr_file
-  exit
 }
 
 spoof_mac() {
-  mac_to_spoof=$1
   if [ $mac_to_spoof = "random" ] ; then
     echo "[INFO] generating a randmom mac ..."
     mac_to_spoof=`openssl rand -hex 6 | sed "s/\(..\)/\1:/g; s/.$//"`
   fi
   echo "[INFO] spoofing your mac as '$mac_to_spoof' on interface '$iface'"
+  $airport_bin $iface -z
   ifconfig $iface ether $mac_to_spoof
-  exit
+  networksetup -setairportnetwork $iface $ap_ssid
+  ifconfig $iface down
+  ifconfig $iface up
 }
 
 show_mac() {
   my_mac=`ifconfig $iface|grep ether|awk '{print $2;}'`
   echo "[INFO] your current mac address on interface $iface is: $my_mac"
-  exit
 }
 
 usage() {
   echo "Usage: $0 [options]"
-  echo "  -i <interface> [default: en0 note: needs to be first argument to choose another interface]"
+  echo "  -i <interface> [default: $iface]"
   echo "  -m <mac_address_to_spoof> [mac address to spoof or \"random\" to generate one]"
   echo "  -s [saves current mac address]"
   echo "  -r [restores previously saved mac address]"
   echo "  -l [shows the currently used mac address]"
+  echo "  -a [access point to reconnect. default: $ap_ssid]"
   exit
 }
 
+#  --- main entry ---
 check_root
 
+# parse commandline
 while getopts "$options_list" opt; do
   case $opt in
     i)
       iface=$OPTARG
       ;;
+    a)
+      ap_ssid=$OPTARG
+      ;;
     m)
-      spoof_mac $OPTARG
+      operation=1
+      mac_to_spoof=$OPTARG
       ;;
     s)
-      save_mac
+      operation=2
       ;;
     r)
-      restore_mac
+      operation=3
       ;;
     l)
-      show_mac
+      operation=4
       ;;
     h)
       usage
@@ -111,4 +125,23 @@ while getopts "$options_list" opt; do
    esac
 done
 
-usage
+# execute the request
+case $operation in 
+  1)
+    # save first before spoofing so we can restore later.
+    save_mac
+    spoof_mac 
+    ;;
+  2)
+    save_mac
+    ;;
+  3)
+    restore_mac
+    ;;
+  4)
+    show_mac
+    ;;
+  *)
+    usage
+    ;;
+esac
