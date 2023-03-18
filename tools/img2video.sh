90ebcd0ed825ed26ca96f6033d3eb81c260cbf0f
Arul Selvan <aselvan@selvans.net>
2023-03-18 14:10:57 -0500

    Create title to video on the fly

--- tools/img2video.sh
+++ tools/img2video.sh
@@ -31,22 +31,35 @@ frame_rate="1/7"
 filter_complex="scale=$scale:force_original_aspect_ratio=decrease,pad=$scale:(ow-iw)/2:(oh-ih)/2"
 image_wildcard="*.jpg"
 audio_file=""
-title_image=""
+title_text=""
 output_file="output.mp4"
+title_image="_title.jpg"
+title_background="blue"
+title_foreground="white"
+title_font="Chalkboard-SE-Bold"
+title_font_size=75
+title_size="1600x1200"
 
 usage() {
-  echo ""
-  echo "Usage: $my_name [options]"
-  echo "  -i <image[s]>    --> input images [default: $image_wildcard]"
-  echo "  -t <title_image> --> title image to use, it must exist on same path and writable [optional]"
-  echo "  -a <mp3>         --> mp3 audio for adding background [optional]"
-  echo "  -s <scale>       --> scale images to width:height [default: $scale]"
-  echo "  -f <framerate>   --> framerate 1/sec [default: $frame_rate]"
-  echo "  -o <output>      --> filename for output video [default: $output_file]"
-  echo ""
-  echo "example: $my_name -t mytitle.png -i \"$image_wildcard\" -a background.mp3 -s $scale -o $output_file"
-  echo ""
+cat << EOF
+  Usage: $my_name [options]
+    -i <image[s]>   --> input images [default: $image_wildcard]
+    -t <text>       --> text can contain '\n' for multi-line to create a title image [optional]
+    -a <mp3>        --> mp3 audio for adding background [optional]
+    -s <scale>      --> scale images to width:height [default: $scale]
+    -f <framerate>  --> framerate 1/sec [default: $frame_rate]"
+    -o <output>     --> filename for output video [default: $output_file]
+  
+   example: $my_name -t "Vacation 2023\nPictures from our vacation" -i "$image_wildcard" -a background.mp3 -s $scale -o $output_file
+
+EOF
   exit 0
+}
+
+# for now hardcoded values, can expand to take arguments for font/image size etc.
+create_title_image() {
+  echo "[INFO] creating video title image `pwd`/$title_image ..." |tee -a $log_file
+  magick -size $title_size -gravity center -background $title_background -fill $title_foreground -font $title_font -pointsize $title_font_size label:"$title_text" $title_image
 }
 
 # ensure ffmpeg is available
@@ -56,7 +69,13 @@ if [ $? -ne 0 ] ; then
   exit 1
 fi
 
+
 # ----------  main --------------
+if [ -f $log_file ] ; then
+  rm $log_file
+fi
+echo "[INFO] $my_version" |tee $log_file
+
 # parse commandline options
 while getopts $options opt; do
   case $opt in
@@ -64,9 +83,8 @@ while getopts $options opt; do
       image_wildcard="$OPTARG"
       ;;
     t)
-      title_image="$OPTARG"
-      # update current time so this file is sorted at the top by -pattern_type glob option
-      touch $title_image
+      title_text="$OPTARG"
+      create_title_image
       ;;
     a)
       audio_file="-i $OPTARG"
@@ -93,12 +111,6 @@ while getopts $options opt; do
   esac
 done
 
-if [ -f $log_file ] ; then
-  rm $log_file
-fi
-
-
-echo "[INFO] $my_version starting ..." |tee $log_file
 echo "[INFO] creating video file using all images found at `pwd`/$image_wildcard ..." |tee -a $log_file
 ffmpeg -framerate $frame_rate -pattern_type glob -i "$image_wildcard" $audio_file $video_codec -filter_complex "$filter_complex" -pix_fmt yuv420p -r 30 -shortest -y $output_file >> $log_file 2>&1
 echo "[INFO] Created video file: `pwd`/$output_file" | tee -a $log_file
