baea7c76001e50f38f08f9b8fafe6bda580299f6
Arul Selvan <aselvan@selvans.net>
2023-03-20 10:14:55 -0500

    Parse CSV and process arguments, still no impl yet

--- tools/create_videos.sh
+++ tools/create_videos.sh
@@ -30,15 +30,14 @@ my_version="`basename $0` v$version"
 host_name=`hostname`
 os_name=`uname -s`
 cmdline_args=`printf "%s " $@`
-
 log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
 options="c:s:vh?"
 verbose=0
-sample_env="${SAMPLE_ENV:-default_value}"
 
 # staging path and it should have ton of freespace to create potentially
 # large video files out of each of the directories. Just use a path that
 # has at least 100GB free space.
+img2video="img2video.sh"
 stage_dir=""
 csv_file=""
 src_path=""
@@ -46,8 +45,8 @@ src_mask=""
 background_mp3_file=""
 video_title=""
 video_name=""
-
 IFS_old=$IFS
+record_count=0
 
 # ensure path for cron runs
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
@@ -94,15 +93,74 @@ init_osenv() {
   fi
 }
 
+assign_img2video_arguments() {
+  # assign img2video.sh arguments
+  if [ ! -z "$video_title" ] ; then
+    i2v_title="-t \"$video_title\""
+  else
+    i2v_title=""
+    echo "### title empty!"
+  fi
+  if [ ! -z "$background_mp3_file" ] ; then
+    i2v_mp3="-a $background_mp3_file"
+  else
+    i2v_mp3=""
+    echo "### mp3 empty"
+  fi
+  if [ ! -z "$src_mask" ] ; then
+    i2v_mask="-i \"$src_mask\""
+  else
+    i2v_mask=""
+    echo "### mask empty"
+  fi
+  if [ ! -z "$video_name" ] ; then
+    i2v_output="-o $video_name"
+  else
+    i2v_output=""
+    echo "### output empty"
+  fi
+}
+
+trim_column_values() {
+  src_path=$(echo $src_path|xargs)
+  src_mask=$(echo $src_mask|xargs)
+  video_title=$(echo $video_title|xargs)
+  video_name=$(echo $video_name|xargs)
+  background_mp3_file=$(echo $background_mp3_file|xargs)
+}
+
 create_video() {
-  write_log "[INFO]" "Creating video $video_name using files from $src_path ..."
+  # validate requirements (if we don't have src path & mask nothing to do)
+  if [ -z "$src_path" ] ; then
+    write_log "[WARN] CSV file missing required argument column 'Directory Path' ... skipping row $record_count"
+    return
+  fi
+  if [ -z "$src_mask" ] ; then
+    write_log "[WARN] CSV file missing required argument 'Image Files' ... skipping row $record_count"
+    return
+  fi
+  write_log "[STAT]" "### Processing row #$record_count ###      "
+  write_log "[INFO]" "  Path: $src_path"
   write_log "[INFO]" "  Mask: $src_mask"
-  write_log "[INFO]" "  Video Title: $video_title"
-  write_log "[INFO]" "  Video Name:  $video_name"
-  write_log "[INFO]" "  Video Background: $background_mp3_file"
+  write_log "[INFO]" "  Title: $video_title"
+  write_log "[INFO]" "  Name:  $video_name"
+  write_log "[INFO]" "  Background: $background_mp3_file"
 
-  # TODO: implement video creation
+  # copy the files from $src_path to the staging area
+  if [ ! -d $src_path ] ; then
+    write_log "[ERROR] Image source directory $src_path does not exist!, skipping row #$record_count"
+    return
+  fi
+  cd $stage_dir || exit
+  # cp -p $src_path/* .
 
+  # setup arguments to call img2video.sh
+  assign_img2video_arguments
+  echo $img2video $i2v_mask $i2v_title $i2v_mp3 $i2v_output
+
+  # execute img2video and wait till it completes
+  write_log "[STAT]" "Calling img2video.sh, it would take a long time, so wait..."
+  
 }
 
 # ----------  main --------------
@@ -139,14 +197,22 @@ if [ ! -d "$stage_dir" ] ; then
   usage
 fi
 
-
 # loop through CSV file and create video for each entry
 exec < $csv_file
 read header
 while IFS="," read -r src_path src_mask background_mp3_file video_title video_name
 do
+  # check for EOF
   if [ -z "$src_path" ] ; then
     continue
   fi
+
+  # row counter
+  ((record_count++))
+
+  # trim whitspace on all column values first
+  trim_column_values
+
+  # finally, create video
   create_video
 done 
