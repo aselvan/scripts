5548db2d778ebb7da31b3d254c542f2859ce472c
Arul Selvan <aselvan@selvans.net>
2023-03-21 14:01:11 -0500

    Use standard logging, verbose/terse support

--- tools/reset_file_timestamp.sh
+++ tools/reset_file_timestamp.sh
@@ -1,36 +1,72 @@
 #!/bin/bash
-  
+#  
 # reset_file_timestamp.sh --- reset a file's timestamp matching the 'createdate' from its metadata
 #
 # This script will read the file metadata from media files like jpeg,mp3 etc using exiftool and
 # reset the OS filename timestamp to match the createdate in the metadata.
 #
 # Author : Arul Selvan
-# Version: Jul 10, 2022
+# Created: Jul 10, 2022
+#
+# Version History
+# --------------
+#   22.07.10 --- Initial version
+#   23.03.21 --- Use stanard logging, with terse support
 
+# version format YY.MM.DD
+version=23.03.21
 my_name=`basename $0`
+my_version="`basename $0` v$version"
 os_name=`uname -s`
-options="p:h?"
+options="p:vh?"
+verbose=0
 log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
 source_path=""
-exiftool_bin="/usr/bin/exiftool"
+
+# ensure path for cron runs
+export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
 usage() {
-  echo ""
-  echo "Usage: $my_name [options]"
-  echo "  -p <path> file/path for single file (or quoted for wildcard) to reset using metadata's timestamp"
-  echo ""
-  echo "example: $my_name -p \"$HOME/*.jpg\""
-  echo ""
+  cat << EOF
+
+  Usage: $my_name [options]"
+    -p <path>   ---> file/path for single file (or quoted for wildcard) to reset using metadata's timestamp"
+    -v <number> ---> Verbose=1 or terse=0 [Default: $verbose]
+  
+  example: $my_name -p \"$HOME/*.jpg\""
+EOF
   exit 0
 }
 
+write_log() {
+  local msg_type=$1
+  local msg=$2
+
+  # log info type only when verbose flag is set
+  if [ "$msg_type" == "[INFO]" ] && [ $verbose -eq 0 ] ; then
+    return
+  fi
+
+  echo "$msg_type $msg" | tee -a $log_file
+}
+init_log() {
+  if [ -f $log_file ] ; then
+    rm -f $log_file
+  fi
+  write_log "[STAT]" "$my_version"
+  write_log "[STAT]" "starting at `date +'%m/%d/%y %r'` ..."
+}
+
 # ----------  main --------------
+init_log
 # parse commandline options
 while getopts $options opt; do
   case $opt in
     p)
       source_path="$OPTARG"
+      ;;
+    v)
+      verbose=1
       ;;
     ?)
       usage
@@ -44,23 +80,14 @@ while getopts $options opt; do
   esac
 done
 
-if [ -f $log_file ] ; then
-  rm $log_file
-fi
-
 if [ -z "$source_path" ] ; then
   usage
 fi
 
-echo "[INFO] $my_name starting ..." > $log_file
-
-if [ $os_name = "Darwin" ]; then
-  exiftool_bin=/usr/local/bin/exiftool
-fi
-
 # ensure exiftool is available
-if [ ! -e $exiftool_bin ] ; then
-  echo "[ERROR] $exiftool_bin is required for this script to work"
+which exiftool >/dev/null 2>&1
+if [ $? -ne 0 ] ; then
+  write_log "[ERROR]" "ffmpeg is required for this script to work, install it first [ex: brew install ffmpeg]."
   exit 1
 fi
 
@@ -76,22 +103,22 @@ fi
 for fname in ${file_list} ;  do
   # if filename is directory, skip
   if [ -d $fname ] ; then
-    echo "[WARN] $fname is a directory, skipping ..." | tee -a $log_file
+    write_log "[WARN]" "$fname is a directory, skipping ..."
     continue
   fi
 
-  create_date=`$exiftool_bin -d "%Y%m%d%H%M.%S" -createdate $fname | awk -F: '{print $2;}'`
+  create_date=`exiftool -d "%Y%m%d%H%M.%S" -createdate $fname | awk -F: '{print $2;}'`
   if [ -z "$create_date" ] ; then
-    echo "[WARN] metadata for $fname does not contain create date, skipping ..." | tee -a $log_file
+    write_log "[WARN]" "metadata for $fname does not contain create date, skipping ..."
     continue
   fi
  
   # validate createdate since sometimes images contain create date but show " 0000"
   if [ "$create_date" = " 0000" ] ; then
-    echo "[WARN] Invalid create date ($create_date) for $fname, skipping ..." | tee -a $log_file
+    write_log "[WARN]" "Invalid create date ($create_date) for $fname, skipping ..."
     continue
   fi
 
-  echo "[INFO] resetting date: touch -t $create_date $fname" | tee -a $log_file
+  write_log "[INFO]" "resetting date: touch -t $create_date $fname"
   touch -t $create_date $fname
 done
