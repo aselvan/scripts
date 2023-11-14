30da5a5ea7027c5e8a28dc0c8e326e922f4b07a0
Arul Selvan <aselvan@selvans.net>
2023-11-14 12:56:30 -0600

    fixed version printing

--- utils/sample.sh
+++ utils/sample.sh
@@ -10,11 +10,13 @@
 # version format YY.MM.DD
 version=23.05.02
 my_name="`basename $0`"
-my_version="`basename $0` v$my_version"
+my_version="`basename $0` v$version"
 my_title="Sample script"
 my_dirname=`dirname $0`
 my_path=$(cd $my_dirname; pwd -P)
 my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
 # commandline options
 options="e:vh?"
@@ -22,14 +24,10 @@ options="e:vh?"
 # ensure path for cron runs
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
-# include logger and functions (requires these files in same path as this script)
-source $my_path/logger.sh
-source $my_path/functions.sh
-
 usage() {
   cat << EOF
 
-  $my_name $my_title
+  $my_name - $my_title
 
   Usage: $my_name [options]
      -e <email>    ---> email address to send success/failure messages
@@ -42,8 +40,20 @@ EOF
   exit 0
 }
 
-# ----------  main --------------
-log.init $my_logfile
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "ERROR: SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  exit 1
+fi
+# init logs
+log.init
+
 #check_installed "required_binary"
 #check_root
 
