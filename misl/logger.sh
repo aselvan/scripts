914c2cdaae2dfeca1918b7aaa107f7a6b8adf099
Arul Selvan <aselvan@selvans.net>
2023-11-14 09:45:09 -0600

    Changed to log to console if file not provided

--- misl/logger.sh
+++ misl/logger.sh
@@ -13,22 +13,23 @@ green=32
 red=31
 blue=34
 
+# note: if logger filename is not provided on log.init by caller, just log to console
+
 # -- Log functions ---
 log.init() {
   logger_file=$1
   if [ $logger_init -eq 1 ] ; then
     return
   fi
-
-  if [ -z $logger_file ] ; then
-    echo "FATAL: log filename is required for logger.sh ... exiting!"
-    exit 1
-  fi
   logger_init=1
-  if [ -f $logger_file ] ; then
+  if [ -f "$logger_file" ] ; then
     rm -f $logger_file
   fi
-  echo -e "\e[0;34m$my_version, `date +'%m/%d/%y %r'` \e[0m" | tee -a $logger_file
+  if [ ! -z "$logger_file" ] ; then
+    echo -e "\e[0;34m$my_version, `date +'%m/%d/%y %r'` \e[0m" | tee -a $logger_file
+  else
+    echo -e "\e[0;34m$my_version, `date +'%m/%d/%y %r'` \e[0m" 
+  fi
 }
 
 log.info() {
@@ -37,16 +38,26 @@ log.info() {
   fi
   log.init
   local msg=$1
-  echo -e "\e[0;32m$msg\e[0m" | tee -a $logger_file 
+  if [ ! -z "$logger_file" ] ; then  
+    echo -e "\e[0;32m$msg\e[0m" | tee -a $logger_file
+  else
+    echo -e "\e[0;32m$msg\e[0m" 
+  fi
 }
+
 log.debug() {
   if [ $verbose -eq 0 ] ; then
     return;
   fi
   log.init
   local msg=$1
-  echo -e "\e[1;30m$msg\e[0m" | tee -a $logger_file 
+  if [ ! -z "$logger_file" ] ; then  
+    echo -e "\e[1;30m$msg\e[0m" | tee -a $logger_file
+  else
+    echo -e "\e[1;30m$msg\e[0m"
+  fi
 }
+
 log.stat() {
   log.init
   local msg=$1
@@ -54,16 +65,29 @@ log.stat() {
   if [ -z $color ] ; then
     color=$blue
   fi
-  echo -e "\e[0;${color}m$msg\e[0m" | tee -a $logger_file 
+  if [ ! -z "$logger_file" ] ; then 
+    echo -e "\e[0;${color}m$msg\e[0m" | tee -a $logger_file 
+  else
+    echo -e "\e[0;${color}m$msg\e[0m"
+  fi
 }
+
 log.warn() {
   log.init
   local msg=$1
-  echo -e "\e[0;33m$msg\e[0m" | tee -a $logger_file 
+  if [ ! -z "$logger_file" ] ; then  
+    echo -e "\e[0;33m$msg\e[0m" | tee -a $logger_file
+  else
+    echo -e "\e[0;33m$msg\e[0m"
+  fi
 }
+
 log.error() {
   log.init
   local msg=$1
-  echo -e "\e[0;31m$msg\e[0m" | tee -a $logger_file 
+  if [ ! -z "$logger_file" ] ; then  
+    echo -e "\e[0;31m$msg\e[0m" | tee -a $logger_file
+  else
+    echo -e "\e[0;31m$msg\e[0m"
+  fi
 }
-
