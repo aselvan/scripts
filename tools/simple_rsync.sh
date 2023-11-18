fb5c3ac4534aa27b7feaa27ede17c8a1cfb19996
Arul Selvan <aselvan@selvans.net>
2023-11-17 19:10:53 -0600

    Updated to use logger includes

--- tools/simple_rsync.sh
+++ tools/simple_rsync.sh
@@ -11,21 +11,19 @@
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
 # version format YY.MM.DD
-version=23.08.28
-my_name=`basename $0`
+version=23.11.17
+my_name="`basename $0`"
 my_version="`basename $0` v$version"
-host_name=`hostname`
-os_name=`uname -s`
+my_title="Sample script"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
+
+# commandline options
 options="s:d:vh"
-
 run_host=`hostname`
-log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
-log_init=0
-verbose=0
-green=32
-red=31
-blue=34
-
 rsync_opts="-rlptoq --ignore-errors --delete --cvs-exclude --temp-dir=/tmp --exclude \"*.vmdk\" --exclude=/root/gdrive"
 IFS_old=$IFS
 
@@ -34,70 +32,32 @@ src_list=""
 backup_dir=""
 
 usage() {
-  cat << EOF
-  Usage: $my_name [options]
-    -s <source_dirs>     --> source directories separated by comma [$src_list]
-    -d <destination_dir> --> destination directory to sync [$backup_dir]
-    -v enable verbose mode
-    -h help
+cat << EOF
+Usage: $my_name [options]
+  -s <source_dirs>     --> source directories separated by comma [$src_list]
+  -d <destination_dir> --> destination directory to sync [$backup_dir]
+  -v enable verbose mode
+  -h help
 
-  example: $my_name -s "/path/dir, path/dir" -d /data/save
+example: $my_name -s "/path/dir, path/dir" -d /data/save
   
 EOF
   exit 0
 }
 
-# -- Log functions ---
-log.init() {
-  if [ $log_init -eq 1 ] ; then
-    return
-  fi
-
-  log_init=1
-  if [ -f $log_file ] ; then
-    rm -f $log_file
-  fi
-  echo -e "\e[0;34m$my_version, `date +'%m/%d/%y %r'` \e[0m" | tee -a $log_file
-}
-
-log.info() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[0;32m$msg\e[0m" | tee -a $log_file 
-}
-log.debug() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[1;30m$msg\e[0m" | tee -a $log_file 
-}
-log.stat() {
-  log.init
-  local msg=$1
-  local color=$2
-  if [ -z $color ] ; then
-    color=$blue
-  fi
-  echo -e "\e[0;${color}m$msg\e[0m" | tee -a $log_file 
-}
-log.warn() {
-  log.init
-  local msg=$1
-  echo -e "\e[0;33m$msg\e[0m" | tee -a $log_file 
-}
-log.error() {
-  log.init
-  local msg=$1
-  echo -e "\e[0;31m$msg\e[0m" | tee -a $log_file 
-}
-
-# ----------  main --------------
-log.init
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "ERROR: SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  exit 1
+fi
+# init logs
+log.init $my_logfile
 
 # parse commandline options
 while getopts $options opt; do
