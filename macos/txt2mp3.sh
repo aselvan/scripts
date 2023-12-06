c106c48692cb67320a7926e3a51336243b5eb195
Arul Selvan <aselvan@selvans.net>
2023-12-06 11:31:59 -0600

    Modified to write mp4 (audio only) incase lame is not available. Also changed to use logging as include

--- macos/txt2mp3.sh
+++ macos/txt2mp3.sh
@@ -1,104 +1,145 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# txt2mp3.sh --- wrapper to create mp3 using macOS tools
+# txt2mp3.sh --- convert text to mp3 using macOS tools
 #
-# The script uses 'say' commandline binary comes w/ MacOS to convert text to AIFF and
-# uses 'lame' to convert AIFF to MP3
+# The script uses 'say' commandline binary that is part of MacOS to convert text to mp3
+# Note:
+#   The say' coommand does not support mp3 so we need 'lame' to convert to mp3. If lame
+#   is not installed, this script creates an audio only mp4 instead of mp3
 #
-# PreReq: lame (install w/ 'brew install lame')
+# PreReq: 
+#   lame (install w/ 'brew install lame')
 #
 # Author:  Arul Selvan
 # Created: Dec 30, 2022
 #
 
 # version format YY.MM.DD
-version=22.12.30
+version=23.12.06
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-cmdline_args=`printf "%s " $@`
-lame_bin="/usr/local/bin/lame"
-my_pid=$$
+my_title="Convert text to mp3 using macOS tools"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
-log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
-options="h?s:o:"
-message=""
-out_file="/tmp/txt2mp3.mp3"
+# commandline options
+options="t:o:l:vh?"
+
+my_pid=$$
+text=""
+volume=30
+model="Samantha (Enhanced)"
+cur_vol=0
+out_file="/tmp/$(echo $my_name|cut -d. -f1).mp3"
+default_format="--file-format mp4f"
 
 # ensure path for cron runs
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
 usage() {
-  echo ""
-  echo "Usage: $my_name [options]"
-  echo "  -s <message>     ---> message in quotes to be converted to mp3"
-  echo "  -o <output_file> ---> output file name for mp3 [default: $out_file]" 
-  echo "  -h               ---> print usage/help"
-  echo ""
-  echo "example: $my_name -s \"Hello [[slnc 75]], my name is Bob\""
-  echo "         the above example inserts 75 msec pause after saying 'Hello'"
-  echo ""
+cat << EOF
+$my_name - $my_title
+
+Usage: $my_name [options]
+  -t <text>     ---> text (can include rate of speach) in quotes to be converted to mp3
+  -o <filename> ---> output file name for mp3 [default: $out_file]
+  -l <number>   ---> volume to adjust before speaking between 1-100 [Default: $volume]
+  -m <model>    ---> voice model in quotes [Default: "$model"]
+  -v            ---> enable verbose, otherwise just errors are printed
+  -h            ---> print usage/help
+  
+example: $my_name -t "Hello [[slnc 75]], my name is Bob"
+         the above example inserts 75 msec pause after saying 'Hello'
+EOF
   exit 0
 }
 
-write_log() {
-  local msg_type=$1
-  local msg=$2
-
-  echo "$msg_type $msg" | tee -a $log_file
+set_volume() {
+  # save current volume to restore, & set new volume
+  cur_vol=$(osascript -e 'output volume of (get volume settings)')
+  log.debug "Saved current volume ($cur_vol)."
+  log.debug "Setting current volume to $volume"
+  osascript -e "set volume output volume $volume" 2>&1 | tee -a $log_file
 }
 
-init_log() {
-  if [ -f $log_file ] ; then
-    rm -f $log_file
-  fi
-  write_log "[STAT]" "$my_version: starting at `date +'%m/%d/%y %r'` ..."
+restore_volume() {
+  log.debug "Restoring current volume"
+  osascript -e "set volume output volume $cur_vol"  2>&1 | tee -a $log_file
 }
 
-# ----------  main --------------
-init_log
-if [ ! -x $lame_bin ] ; then
-  write_log "[ERROR]" "required utility $lame_bin is missing, install w/ 'brew install lame'"
-  usage
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+else
+  echo "ERROR: SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  exit 1
 fi
+# init logs
+log.init $my_logfile
+
 # parse commandline options
 while getopts $options opt; do
   case $opt in
-    s)
-      message="$OPTARG"
+    t)
+      text="$OPTARG"
       ;;
     o)
       out_file="$OPTARG"
       ;;
-    ?)
-      usage
+    l)
+      volume="$OPTARG"
       ;;
-    *)
+    m)
+      model="$OPTARG"
+      ;;
+    v)
+      verbose=1
+      ;;
+    ?|h|*)
       usage
       ;;
   esac
 done
 
 # check for message
-if [ -z "$message" ] ; then
-  write_log "[ERROR]" "need text to convert to mp3!"
+if [ -z "$text" ] ; then
+  log.error "Required argument -t missing!"
   usage
 fi
 
-write_log "[INFO]" "converting text to mp3 ..."
-temp_file=$(mktemp /tmp/${my_name}.${my_pid})
+set_volume
 
-echo "$message" | say -vDaniel -o $temp_file
-if [ $? -ne 0 ] ; then
-  write_log "[ERROR]" "converting to AIFF failed!"
-  exit 1
+log.stat "Converting text to mp3..."
+# if lame utility is there, convert to mp3 otherwise default to mp4 (audio only)
+which lame2 2>&1 >/dev/null
+if [ $? -eq 0 ] ; then
+  temp_file=$(mktemp /tmp/${my_name}.${my_pid})
+  echo "$text" | say -v "$model" -o $temp_file 2>&1 >> $my_logfile
+  if [ $? -ne 0 ] ; then
+    log.error "Say tool failed!"
+    rm $temp_file
+    restore_volume
+    exit 2
+  fi
+
+  lame $temp_file $out_file 2>&1 >> $my_logfile
+  if [ $? -ne 0 ] ; then
+    log.error "Converting to MP3 failed!"
+    rm $temp_file
+    restore_volume
+    exit 3
+  fi
+  rm $temp_file
+else
+  # lame tool does not exist, so writing mp4 w/ audio only
+  log.debug "lame tool does not exist, so writing mp4 w/ audio only"
+  echo "$text" | say $default_format -v "$model" -o $out_file  
 fi
 
-$lame_bin $temp_file $out_file
-if [ $? -ne 0 ] ; then
-  write_log "[ERROR]" "converting to MP3 failed!"
-  exit 2
-fi
-
-rm $temp_file
-write_log "[INFO]" "successfully converted message to mp3. The output file is at: $out_file"
-
+restore_volume
