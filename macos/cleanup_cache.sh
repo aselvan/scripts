085c67d1e4f29eaced33f2789dc211eb9885d03e
Arul Selvan <aselvan@selvans.net>
2023-12-13 17:51:15 -0600

    Restructure and refactor to support multiple users

--- macos/cleanup_cache.sh
+++ macos/cleanup_cache.sh
@@ -1,9 +1,9 @@
-#/bin/sh
+#!/usr/bin/env bash
 #
-# cleanup_cache.sh --- empty macOS cache, logs etc 
+# cleanup_cache.sh --- Wipe MacOS cache, logs etc 
 #
 # This script empties logs & cache. Especially cache directory in macOS tend to 
-# grow a lot depending usage. I often find it to gobble up a gig or more after
+# grow a lot depending on usage. I often find it to gobble up a gig or more after
 # a few weeks or so usage, not sure how much it grows if you let it go, but I am 
 # not gonna let it. It builds up the cache as you start using apps anyways.
 #
@@ -11,80 +11,121 @@
 # Version: Jun 14, 2020
 #
 
+# version format YY.MM.DD
+version=23.12.13
+my_name="`basename $0`"
+my_version="`basename $0` v$version"
+my_title="Wipe macOS cache, logs etc"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
+
 # commandline arguments
-options_list="usah"
-
-my_name=`basename $0`
-# works with user login or elevated run
-user=`who -m | awk '{print $1;}'`
-
+options_list="u:sahv"
+current_user=""
+user_list="" # empty for current user i.e. no sudo needed
+do_system=0  # by default just do user level only
 
 usage() {
-  echo "Usage: $my_name [-u|-s|-a]"
-  echo "   -u clean user level only"
-  echo "   -s clean at systemlevel (requires sudo password)"
-  echo "   -a clean user & systemlevel (requires sudo password)"
+  cat << EOF
+$my_name - $my_title
+
+Usage: $my_name [options]
+  -u <list>  ---> List of users to clean [default: only current users cache is cleaned]
+  -s         ---> Enable cleaning system level cache/logs as well
+  -v         ---> enable verbose, otherwise just errors are printed
+  -h         ---> print usage/help
+
+example: $my_name 
+example: $my_name -u "user1 user2 user3" -s
+  
+EOF
   exit 0
 }
 
-check_root() {
-  if [ `id -u` -ne 0 ] ; then
-    echo "[ERROR] you must be 'root' to run '$my_name' ... exiting."
-    exit
-  fi
-}
 
 clean_user() {
-  cache_dir="$HOME/Library/Caches"
-  log_dir="$HOME/Library/Logs"
+  local user=$1
+  local user_home="/Users/$user"
+  cache_dir="${user_home}/Library/Caches"
+  log_dir="${user_home}/Library/Logs"
   
-  echo "[INFO] cleaning $cache_dir ..."
+  # ensure the cache directory exists
+  if [ ! -d $cache_dir ] ; then
+    log.error "  The user '$user' does not have cache dir, possibly non-existent user? skipping..."
+    return
+  fi
+  log.stat "  cleaning at user level $cache_dir ..."
   rm -rf $cache_dir/*
 
-  echo "[INFO] cleaning $log_dir ..."
+ # ensure the cache directory exists
+  if [ ! -d $log_dir ] ; then
+    log.error "  The user '$user' does not have log dir, possibly non-existent user? skipping..."
+    return
+  fi
+  log.stat "  cleaning at user level $log_dir ..."
   rm -rf $log_dir/*
-
 }
 
 clean_system() {
   cache_dir="/Library/Caches"
   log_dir="/Library/Logs"
   
-  echo "[INFO] cleaning $cache_dir ..."
+  log.stat "  cleaning at system level $cache_dir ..."
   rm -rf $cache_dir/*
 
-  echo "[INFO] cleaning $log_dir ..."
+  log.stat "  cleaning at system level $log_dir ..."
   rm -rf $log_dir/*
 }
+
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "ERROR: SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  exit 1
+fi
+# init logs
+log.init $my_logfile
 
 # process commandline
 while getopts "$options_list" opt; do
   case $opt in
     u)
-      echo "[INFO] cleaning cache, logs at user ($user) level..."
-      clean_user
-      exit 0
+      user_list="$OPTARG"
+      check_root
       ;;
     s)
-      echo "[INFO] cleaning cache, logs at system level..."
+      do_system=1
       check_root
-      clean_system
-      exit 0
       ;;
-    a)
-      echo "[INFO] cleaning cache, logs at ALL levels ..."
-      check_root
-      clean_user
-      clean_system
-      exit 0
+    v)
+      verbose=1
       ;;
-    h)
+    ?|h|*)
       usage
       ;;
-    *)
-     usage
-     ;;
    esac
 done
 
-usage
+# if user list is not provided, user current user by default
+if [ -z "$user_list" ] ; then
+  user_list=$(get_current_user)
+fi
+
+for user in $user_list ; do
+  log.stat "cleaning cache, logs for user: $user ..." $green
+  clean_user $user
+done
+
+# clean system if requested
+if [ $do_system -eq 1 ] ; then
+  log.stat "cleaning cache, logs at system level ..." $green
+  clean_system
+fi
