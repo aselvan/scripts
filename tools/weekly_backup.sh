9a97c70a1affcb5e15228052ded2faf715b94493
Arul Selvan <aselvan@selvans.net>
2024-01-14 11:24:07 -0600

    Added code to measure elapsed time for each step and overall steps

--- tools/weekly_backup.sh
+++ tools/weekly_backup.sh
@@ -10,10 +10,10 @@
 #
 
 # version format YY.MM.DD
-version=23.12.31
+version=24.01.14
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-my_title="Weekly back of selvans.net"
+my_title="Weekly backup of selvans.net"
 my_dirname=`dirname $0`
 my_path=$(cd $my_dirname; pwd -P)
 my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
@@ -27,31 +27,25 @@ options_list="e:o:h"
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
 title="selvans.net weekly backup"
-run_host=`hostname`
 desc="Weekly backup log file"
 sed_st="s/__TITLE__/$title/g;s/__DESC__/$desc/g"
 www_root=/var/www
 std_header=${www_root}/std_header.html
 std_footer=${www_root}/std_footer.html
 html_file=${www_root}/weekly_backup_log.html
-
-email_address=""
-subject="trex backup results"
-subject_failed="Weekly backup FAILED"
-subject_space_low="Weekly backup LOWSPACE"
 usb_mount=/media/usb-portable
 backup_dir=$usb_mount/backup
 rsync_opts="-rlptgoq --ignore-errors --delete --cvs-exclude --temp-dir=/data/tmp --exclude \"*.vmdk\" --exclude=/root/gdrive"
 rsync_bin="/usr/bin/rsync"
 # when a device is 90% full, send a nag email
 space_limit_percent=90
-backup_failed=0
+backup_status=0
 offsite_device="/media/usb-ssd-500g"
 IFS_old=$IFS
 
 # list of devices: descriptive name and mount points. NOTE: the /etc/fstab
 # entry should be setup to right device for each of the mount point specified.
-device_names=("Primary,/media/usb-1tb-2" "Secondary,/media/usb-1tb-3" "Tertiary eSATA-RAID,/media/sata-3tb" "Offsite SSD,$offsite_device" )
+device_names=("PRIMARY,/media/usb-1tb-2" "SECONDARY,/media/usb-1tb-3" "TERTIARY eSATA-RAID,/media/sata-3tb" "OFFSITE SSD,$offsite_device" )
 
 usage() {
   cat << EOF
@@ -80,7 +74,9 @@ create_html_log() {
 }
 
 do_backup() {
-  echo "Backup job start: `date`" >> $my_logfile
+  # for use in calculating elapsed time for each step in this
+  local start_time=$SECONDS
+
   echo "Checking backup device: $usb_mount" >> $my_logfile
   # mount the usb if it is not mounted already
   if [ ! -d $usb_mount/backup ];  then
@@ -101,66 +97,78 @@ do_backup() {
   echo "Free space left in device: <b><font color=\"blue\">$free_space</font></b>" >> $my_logfile
   echo "" >> $my_logfile
   if [ $used_percent -gt $space_limit_percent ] ; then
-      free_percent=`expr 100 - $used_percent`
-      echo "<b><font color=\"red\">WARNING: low on space, only ${free_percent}% left on device</font></b>" >> $my_logfile
-      if [ ! -z $email_address ] ; then
-        echo "Backup will continue, and results will be mailed again. This mail is just for awareness" >> $my_logfile
-        /bin/cat $my_logfile | /usr/bin/mail -s "$subject_space_low" $email_address
-      fi
+    free_percent=`expr 100 - $used_percent`
+    echo "<b><font color=\"red\">WARNING: low on space, only ${free_percent}% left on device</font></b>" >> $my_logfile
+    echo "Backup will continue, and results will be mailed again. This mail is just for awareness" >> $my_logfile
+    send_mail "2"
   fi
 
-  # start backup
-  echo "Starting rsync backup (target=$backup_dir) ..." >> $my_logfile
-  echo "    Backup of /etc  ... `date`" >> $my_logfile
+  # start backup (time each step)
+  start_time=$SECONDS
+  echo "Starting rsync (target=$backup_dir) ..." >> $my_logfile
+  echo -n "    Backup of /etc  ... " >> $my_logfile
   nice -19 $rsync_bin $rsync_opts /etc $backup_dir
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "    Backup of /root  ... `date`" >> $my_logfile
+  start_time=$SECONDS
+  echo -n "    Backup of /root  ... " >> $my_logfile
   nice -19 $rsync_bin $rsync_opts /root $backup_dir
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "    Backup of /home  ... `date`" >> $my_logfile
+  start_time=$SECONDS
+  echo -n "    Backup of /home  ... " >> $my_logfile
   nice -19 $rsync_bin $rsync_opts /home $backup_dir
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "    Backup of /var/www  ... `date`" >> $my_logfile
+  start_time=$SECONDS
+  echo -n "    Backup of /var/www  ... " >> $my_logfile
   nice -19 $rsync_bin $rsync_opts /var/www $backup_dir
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "    Backup of /data/videos4youtube  ... `date`" >> $my_logfile
+  echo -n "    Backup of /data/videos4youtube  ... " >> $my_logfile
   nice -19 $rsync_bin $rsync_opts /data/videos4youtube $backup_dir
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "    Backup of /data/debbie-backup  ... `date`" >> $my_logfile
+  echo -n "    Backup of /data/debbie-backup  ... " >> $my_logfile
   nice -19 $rsync_bin $rsync_opts /data/debbie-backup $backup_dir
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
   # Skip this for offsite storage as we may have sensitive information
   # For off-site device copy just the encrypted container.
+  start_time=$SECONDS
   if [ "$usb_mount" != "$offsite_device" ] ; then
-    echo "    Backup of /data/transfer ... `date`" >> $my_logfile
+    echo -n "    Backup of /data/transfer ... " >> $my_logfile
     nice -19 $rsync_bin $rsync_opts /data/transfer $backup_dir
   else
-    echo "    Off-site backup /data/transfer/arul-backup/data/encrypted" >> $my_logfile
+    echo -n "    Off-site backup /data/transfer/arul-backup/data/encrypted" >> $my_logfile
     nice -19 $rsync_bin $rsync_opts /data/transfer/arul-backup/data/encrypted $backup_dir
   fi
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "    Backup of UFW config (/lib/ufw) ... `date`" >> $my_logfile
+  start_time=$SECONDS
+  echo -n "    Backup of UFW config (/lib/ufw) ... " >> $my_logfile
   nice -19 $rsync_bin $rsync_opts /lib/ufw $backup_dir
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "    Backup of root's crontab ..." >> $my_logfile
-	crontab -l > $backup_dir/${run_host}_root.crontab
+  start_time=$SECONDS
+  echo -n "    Backup of root's crontab ..." >> $my_logfile
+	crontab -l > $backup_dir/${host_name}_root.crontab
+  echo "$(($SECONDS - $start_time)) second(s)." >> $my_logfile
 
-  echo "Backup end: `date`" >> $my_logfile
   cp $my_logfile $backup_dir
-  echo "Backup done, now synching ... `date`" >> $my_logfile
+  echo "Backup done, now synching ... " >> $my_logfile
 
   # sync and umount. (umount probably does sync but just be sure)
   sync
-  echo "Last step, unmounting ... `date`" >> $my_logfile
+  echo "Last step, unmounting device ... " >> $my_logfile
   /bin/umount $usb_mount
-  echo "" >> $my_logfile
   return 0
 }
 
 # -------------------------------  main -------------------------------
 # First, make sure scripts root path is set, we need it to include files
 if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
-  # include functions, we dont need logger for this specific script as it has html code
+  source $scripts_github/utils/logger.sh
   source $scripts_github/utils/functions.sh
 else
   echo "ERROR: SCRIPTS_GITHUB env variable is either not set or has invalid path!"
@@ -168,11 +176,13 @@ else
   exit 1
 fi
 
-echo "selvans.net Weekly backup" > $my_logfile
-echo "=========================" >> $my_logfile
+# init logs
+log.init 
+echo "selvans.net --- Weekly backup log" > $my_logfile
+echo "=================================" >> $my_logfile
 echo "" >> $my_logfile
 echo "Script: $my_version" >> $my_logfile
-echo "Host:   $run_host" >> $my_logfile
+echo "Host:   $host_name" >> $my_logfile
 echo "Date:   `date +'%b %d, %Y'`" >> $my_logfile
 
 # ------ main -------------
@@ -185,20 +195,14 @@ while getopts "$options_list" opt; do
     o)
       offsite_device="$OPTARG"
       ;;
-    h)
+    ?|h|*)
       usage
       ;;
-    \?)
-     usage
-     ;;
-    :)
-     usage
-     ;;
    esac
 done
 
 # reset the list in case offsite_device is provided as cmdline option
-device_names=("Primary,/media/usb-1tb-2" "Secondary,/media/usb-1tb-3" "Tertiary eSATA-RAID,/media/sata-3tb" "Offsite SSD,$offsite_device" )
+device_names=("PRIMARY,/media/usb-1tb-2" "SECONDARY,/media/usb-1tb-3" "TERTIARY eSATA-RAID,/media/sata-3tb" "OFFSITE SSD,$offsite_device" )
 
 for devpair in "${device_names[@]}" ; do
   IFS=,
@@ -209,28 +213,29 @@ for devpair in "${device_names[@]}" ; do
   backup_dir=$usb_mount/backup
   echo "" >> $my_logfile
   echo ">>>>>>> $usb_desc backup target device/path: $backup_dir <<<<<<<<< " >> $my_logfile
+  
+  device_start_time=$SECONDS
   do_backup
   tb=$?
   if [ $tb -ne 0 ]; then
     echo "    ERROR: ${keyval[0]} backup failed!" >> $my_logfile
-    backup_failed=1
+    backup_status=1
   fi
+  echo "Total elapsed time: <b>$(($SECONDS - $device_start_time))</b> seconds." >> $my_logfile
+  echo "" >> $my_logfile
 done
 
 # mail good if all backup devices are successful, otherwise even if one fails send bad mail
 echo "" >> $my_logfile
-if [ $backup_failed -eq 0 ] ; then
+if [ $backup_status -eq 0 ] ; then
     echo "<b>Backup Result:</b> <b><font color=\"blue\">SUCCESS</font></b> on all devices" >> $my_logfile
-    if [ ! -z $email_address ] ; then
-      /bin/cat $my_logfile | /usr/bin/mail -s "$subject" $email_address
-    fi
 else
     echo "<b>Backup Result:</b> <b><font color=\"red\">FAILED</font></b> on one or more devices" >> $my_logfile
-    if [ ! -z $email_address ] ; then    
-      /bin/cat $my_logfile | /usr/bin/mail -s "$subject_failed" $email_address
-    fi
 fi
-echo "<b>Total runtime:</b> $(elapsed_time)" >> $my_logfile
+echo "<b>Total backup runtime:</b> $(elapsed_time)" >> $my_logfile
+
+# mail the report 
+send_mail "$backup_status" "$my_logfile"
 
 # write html log
 create_html_log
