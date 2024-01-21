e37f91c3dc225a625f05b63c8a12ee0e66f0d461
Arul Selvan <arul@selvansoft.com>
2024-01-21 13:03:25 -0600

    Refactor to use logger and functions includes

--- security/encrypted_drive.sh
+++ security/encrypted_drive.sh
@@ -1,23 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # encrypted_drive.sh
 #
 # summary: script to store/retrieve files to/from encrypted folder
 # 
-# This script mounts a encrypted drive at specified path and copy your files to the  
-# encrypted drive, and unmounts it. Optionally with -l option, it simply does a directory 
-# listing of the encrypted files as well as -m causes to stay mounted for a period of time. 
-# The script will create two directories (encrypted, decrypted) on drive/path if they don't 
-# exist. The 'encrypted' directory contains the encrpted files which you can backup to any 
-# backup mediums (external drive, even cloud backup like google drive, dropbox etc safely) 
-# for safe keeping. To access the encrupted data when needed, you can run this script with 
-# -m option which mounts a 'decrypted' directory through the encfs driver to expose them as 
-# as normal files inside that directory note: it automatically unmounts after 5 minutes but you 
-# can, optionaly set a longer or shorter time with -i option. On MacOS, when the decrypted drive
+# This script mounts a encrypted drive at specified path and copy your files to the encrypted 
+# drive, and unmounts it. Optionally with -l option, it simply does a directory listing of 
+# the encrypted files as well as -m causes to stay mounted for a period of time. The script 
+# will create two directories (encrypted, decrypted) on drive/path if they don't exist. 
+# The 'encrypted' directory contains the encrpted files which you can backup to any backup 
+# mediums (external drive, even cloud backup like google drive, dropbox etc safely) for safe 
+# keeping. To access the encrupted data when needed, you can run this script with -m option 
+# which mounts a 'decrypted' directory through the encfs driver to expose them as as normal 
+# files inside that directory note: it automatically unmounts after 5 minutes but you can, 
+# optionaly set a longer or shorter time with -i option. On MacOS, when the decrypted drive 
 # is in mounted state, you will have a mounted drive icon on your desktop for conveniently 
 # copying your files to the drive while it is in mounted state. On the first time use of this
 # script on a specific drive/path, it will ask you to choose a passphrase to encrypt files, 
-# make sure you remember that because you need that later when you want to access the encrypted drive.
+# make sure you remember that because you need that later when you want to access the 
+# encrypted drive.
 #
 # Author:  Arul Selvan
 # Version: Jun 23, 2018
@@ -44,9 +45,27 @@
 #
 # NOTE: Change the default enc_drive below to your drive/path before using this script.
 #
+# Version History:
+#   Jun 23, 2018  --- Original version
+#   Jan 21, 2020  --- Refactor to use logger and functions includes
+#
+
+# version format YY.MM.DD
+version=24.01.21
+my_name="`basename $0`"
+my_version="`basename $0` v$version"
+my_title="Sample script"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
+
+# commandline options
+options_list="c:d:i:lmuh"
+
 #enc_drive="/Volumes/exfat64g2"
 enc_drive="/Users/arul/data/personal/keys/encfs"
-options_list="c:d:i:lmuh"
 option=0
 file_to_copy=""
 mounted=0
@@ -54,7 +73,8 @@ idle_minutes=15
 
 usage() {
   cat <<EOF
-  
+$my_name - $my_title
+
 Usage: encrypted_drive.sh [-d /my/encrypted/partition] -c <secret_file> | -muli
   -d  <drive_path>  use the path provided as root. 
       NOTE: this must be the first option
@@ -85,10 +105,10 @@ check_mounted() {
   mount | grep $enc_drive/decrypted
   rc=$?
   if [ $rc -eq 0 ]; then
-    echo "[INFO] $enc_drive/decrypted is mounted already!"
+    log.stat "$enc_drive/decrypted is mounted already!"
     mounted=1
   else
-    echo "[INFO] $enc_drive/decrypted is not mounted, attempting to mount."
+    log.stat "$enc_drive/decrypted is not mounted, attempting to mount."
     mounted=0
   fi
 }
@@ -103,34 +123,46 @@ do_mount() {
   
   encfs --idle=$idle_minutes $enc_drive/encrypted $enc_drive/decrypted
   if [ $? -ne 0 ] ; then
-    echo "[ERROR]: failed to mount $enc_drive path!"
+    log.error "Error: failed to mount $enc_drive path!"
     exit
   fi
   mounted=1
-  echo "[INFO]: mounted $enc_drive/decrypted successfully"
-  echo "[INFO]: $enc_drive/decrypted will stay mounted for $idle_minutes minute(s) and will auto unmount."
+  log.stat "Mounted $enc_drive/decrypted successfully"
+  log.stat "$enc_drive/decrypted will stay mounted for $idle_minutes minute(s) and will auto unmount."
 }
 
 copy() {
   do_mount
-  echo "[INFO]: copying $file_to_copy to $enc_drive/decrypted ..."  
+  log.stat "Copying $file_to_copy to $enc_drive/decrypted ..."  
   cp $file_to_copy $enc_drive/decrypted
 }
 
 list() {
   do_mount
-  echo "-------------------------------------------------"
-  echo "[INFO]: Contents of $enc_drive/encrypted ... "
+  log.stat "-------------------------------------------------"
+  log.stat "Contents of $enc_drive/encrypted ... "
   ls -lthR $enc_drive/decrypted
-  echo "-------------------------------------------------"
+  log.stat "-------------------------------------------------"
 }
 
 do_unmount() {
-  echo "[INFO]: unmounting $enc_drive/decrypted ... "    
+  log.stat "mounting $enc_drive/decrypted ... "    
   umount $enc_drive/decrypted
 }
 
-# -------------- main ---------------------
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "ERROR: SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  exit 1
+fi
+# init logs
+log.init $my_logfile
 
 while getopts "$options_list" opt; do
   option=1
@@ -170,6 +202,6 @@ done
 
 if [ $option -eq 0 ]; then
   echo ""
-  echo "[ERROR] No option provided!"
+  log.warn "No option provided!"
   usage
 fi
