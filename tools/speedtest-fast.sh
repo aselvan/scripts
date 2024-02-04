51c6e2e5a8ae53f429b258090ad96a6e60d7f60f
Arul Selvan <aselvan@selvans.net>
2024-02-04 11:26:18 -0600

    Changed to use common function/logger; optionally create HTML file etc

--- tools/speedtest-fast.sh
+++ tools/speedtest-fast.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
-# speedtest-fast.sh --- wrapper script for netflix's 'fast' test tool to measure speed.
+# speedtest-fast.sh --- Measure bandwidth using netflix provided fast.com.
 #
 # This also calculates what is the average of X runs (passed as commandline arg), 
 # in addition, also creates a HTML file to be displayed on a website which contains
@@ -9,12 +9,26 @@
 # Pre Req: needs netflix's fast commandline impl ex: snap install fast [on ubuntu]
 #
 # Author:  Arul Selvan
-# Version: Oct 3, 2021
-#
+# Version History: 
+#   Oct 3, 2021  --- Initial version
+#   Feb  4, 2024 --- Added functionality to create HTML file, changed output format
 
-my_name=`basename $0`
-run_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
-options_list="e:n:s:r:w:h"
+# version format YY.MM.DD
+version=24.02.04
+my_name="`basename $0`"
+my_version="`basename $0` v$version"
+my_title="Measure bandwidth using netflix provided fast.com"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
+
+# ensure path for cron runs
+export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
+
+# commandline options
+options_list="e:n:s:r:d:wvh?"
 
 # For HTML file content (change as needed)
 title="selvans.net speed test results"
@@ -22,55 +36,99 @@ desc="This file contains selvans.net speed test measured by netflix provided fas
 sed_st="s/__TITLE__/$title/g;s/__DESC__/$desc/g"
 
 # location of www path, speedtest history file etc.
+need_html=0
 home_dir=/root/speed_test
 www_root=/var/www
-speedtest_outfile=$home_dir/speed_test.log
-log_file=$home_dir/speed_test.txt
-log_file_reverse=$home_dir/speed_test_reverse.txt
 html_file=$home_dir/speed_test.html
 std_header=$www_root/std_header.html
 std_footer=/var/www/std_footer.html
 
+fast_outfile="/tmp/$(echo $my_name|cut -d. -f1)_fast.log"
+log_file=$my_logfile
+log_file_reverse="/tmp/$(echo $my_name|cut -d. -f1)_reverse.log"
 line_count=1
 total=0
 average=0
 dl=""
-speedtest_bin="/snap/bin/fast"
-
-# email details
-email_subject="SpeedTest low speed detected"
-email_address=""
 low_speed=50 # anything below 50Mbit considered low speed
 nrun=18
 retry_count=1
 retry_wait=60
-os_name=`uname -s`
 
 usage() {
-  echo "Usage: $my_name [options]"
-  echo "  -e <email_address> --- email address to send results"
-  echo "  -n <number>        --- number of last runs to calculate average [default: $nrun]"
-  echo "  -s <number>        --- speed in mbit below this number is assumed low speed [default: $low_speed]"
-  echo "  -r <number>        --- number of attempts in case fast.com is not responding [default: $retry_count]"
-  echo "  -w <number>        --- number of seconds to wait between attempts [default: $retry_wait]"
+cat << EOF
+$my_name - $my_title
+
+Usage: $my_name [options]
+  -e <email>  ---> email address to send results
+  -n <number> ---> number of last runs to calculate average [default: $nrun]
+  -s <number> ---> speed in mbit below this number is assumed low speed [default: $low_speed]
+  -r <number> ---> number of attempts in case fast.com is not responding [default: $retry_count]
+  -d <number> ---> number of seconds to delay/wait between attempts [default: $retry_wait]
+  -w          ---> writes HTML file ($html_file) in addition for web server display.
+
+  example: $my_name -r3 -e foo@bar.com
+
+EOF
   exit 0
 }
 
 do_speedtest() {
   local result=0
   # run the test
-  echo "[INFO] running $speedtest_bin ... " |/usr/bin/tee -a $run_logfile
-  $speedtest_bin  > $speedtest_outfile 2>&1
+  log.debug "running fast ... "
+  fast  > $fast_outfile 2>&1
   if [ $? -ne 0 ] ; then
-    echo "[ERROR] non-zero exit running '$speedtest_bin', bailing out..." |/usr/bin/tee -a $run_logfile
+    log.error "non-zero exit running fast, bailing out..."
     exit 1
   fi
 
-  echo "[INFO] parsing results ..." | /usr/bin/tee -a $run_logfile
+  log.debug "parsing results ..."
   # fast (go implementation by ddooo) writes a ticker with spinning graphic on console
   # we capture that to a file and use awk to get the last line which is the total download speed
-  dl=$(cat $speedtest_outfile|awk -F'>' '{ print $2;}'|awk '{print $1;}')
+  dl=$(cat $fast_outfile|awk -F'>' '{ print $2;}'|awk '{print $1;}')
 }
+
+write_html() {
+  # calculate average for the last nrun 
+  log.stat "calculate average for last $nrun runs..."
+  tac $log_file > $log_file_reverse
+  while IFS= read -r line ; do
+	  ((line_count++))
+    if [ $line_count -ge $nrun ]; then
+		  break
+	  fi
+	  dl_avg=$(echo $line|awk '{print $6}')
+	  total=$(echo "$total + $dl_avg"|/usr/bin/bc)
+  done < $log_file_reverse
+  average=$(echo "$total / $nrun"|/usr/bin/bc)
+
+  # prepare the HTML file for website
+  log.stat "creating HTML file ($html_file) ..."
+  cat $std_header| sed -e "$sed_st"  > $html_file
+  echo "<body><pre>" >> $html_file
+  echo "<h3>$my_version --- bandwidth test measured by netflix provided fast.com </h3>" >> $html_file
+  echo "<b>Current speed:</b> $dl Mbps <br>" >> $html_file
+  echo "<b>Average of last $nrun runs:</b> $average Mbps<br><br>" >>$html_file
+  tac $log_file  >> $html_file
+  echo "</pre>" >> $html_file
+  cat $std_footer >> $html_file
+  mv $html_file ${www_root}/.
+}
+
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "ERROR: SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  exit 1
+fi
+# init logs
+log.init $my_logfile
 
 # ------ main -------------
 # process commandline
@@ -88,44 +146,47 @@ while getopts "$options_list" opt; do
     r)
       retry_count=$OPTARG
       ;;
-    w)
+    d)
       retry_wait=$OPTARG
       ;;
-    h)
+    w)
+      need_html=1
+      ;;
+    v)
+      verbose=1
+      ;;
+    ?|h|*)
       usage
       ;;
-    \?)
-     usage
-     ;;
-    :)
-     usage
-     ;;
    esac
 done
 
 ts=$(date +"%D %H:%M %p")
-echo "[INFO] $my_name starting at [$ts] ... " |/usr/bin/tee $run_logfile
-if [ -f $speedtest_outfile ] ; then
-  rm $speedtest_outfile
+if [ -f $fast_outfile ] ; then
+  rm $fast_outfile
 fi
 
-if [ ! -x $speedtest_bin ]; then
-  echo "[ERROR] required tool $speedtest_bin is missing!" | /usr/bin/tee -a $run_logfile
+# make sure fast exists
+check_installed fast
+
+# reset output to permanant store rather then /tmp/ in the case of html runs
+if [ $need_html -ne 0 ] ; then
+  log_file=$home_dir/speed_test.txt
 fi
 
 # attempt $retry_count times
 for (( attempt=0; attempt<$retry_count; attempt++)) {
-  echo "[INFO] speed test attempt #$attempt ..." | /usr/bin/tee -a $run_logfile
+  log.stat "speed test attempt #$attempt ..."
   do_speedtest
   if [[ -z $dl || "$dl" = "0" ]] ; then
-    echo "[INFO] sleeping $retry_wait seconds ..." | /usr/bin/tee -a $run_logfile
+    log.stat "sleeping $retry_wait seconds ..."
     sleep $retry_wait
   else
     break
   fi
 }
 
-echo "[INFO] parsed download measure: '$dl' Mbps" | /usr/bin/tee -a $run_logfile
+log.stat "Download speed: $dl Mbps"
 speedtest_output="[$ts] measured bandwidth: $dl Mbps (download) ; N/A Mbps (upload) ; N/A ms (ping)"
 if [ -z $dl ] ; then
   echo "[$ts] Unexpected output 0 " >> $log_file 
@@ -133,37 +194,15 @@ else
 	echo $speedtest_output >> $log_file
 fi
 
-# calculate average for the last nrun 
-echo "[INFO] calculate average for last $nrun runs..." |/usr/bin/tee -a $run_logfile
-tac $log_file > $log_file_reverse
-while IFS= read -r line ; do
-	((line_count++))
-  if [ $line_count -ge $nrun ]; then
-		break
-	fi
-	dl_avg=$(echo $line|awk '{print $6}')
-	total=$(echo "$total + $dl_avg"|/usr/bin/bc)
-done < $log_file_reverse
-average=$(echo "$total / $nrun"|/usr/bin/bc)
-
-# prepare the HTML file for website
-echo "[INFO] creating HTML file ($html_file) ..." |/usr/bin/tee -a $run_logfile
-cat $std_header| sed -e "$sed_st"  > $html_file
-echo "<body><pre>" >> $html_file
-echo "<h2>Speed Test: Measured with Netflix provided fast.com testing tool</h2>" >> $html_file
-echo "<h3>$speedtest_bin </h3>" >> $html_file
-echo "<h3>Average of last $nrun runs: $average Mbps</h3>" >>$html_file
-tac $log_file  >> $html_file
-echo "</pre>" >> $html_file
-cat $std_footer >> $html_file
-mv $html_file ${www_root}/.
+# create HTML with stats if requested
+if [ $need_html -ne 0 ] ; then
+  write_html
+fi
 
 # finally, mail if we found speed is lower than the low_speed threshold
 # first, convert $dl to integer for comparison
 dl_int=$( printf "%.0f" $dl )
-if [[ $dl_int -le $low_speed && ! -z $email_address ]] ; then
-  echo "[WARN] low speed detected: $dl_int Mbps is < $low_speed Mbps, so sending e-mail ..." | /usr/bin/tee -a $run_logfile
-  cat $run_logfile | /usr/bin/mail -s "$email_subject" $email_address
+if [ $dl_int -le $low_speed ] ; then
+  log.warn "low speed detected: $dl_int Mbps is < $low_speed Mbps, so sending e-mail ..."
+  send_mail 0 
 fi
-echo "[INFO] all done." |/usr/bin/tee -a $run_logfile
-
