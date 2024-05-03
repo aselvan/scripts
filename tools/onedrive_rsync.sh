f9ffad76c0e21d12e25ef2562bde39b71ef7c30d
Arul Selvan <aselvan@selvans.net>
2024-05-03 12:26:24 -0500

    refactor to use rclone sync instead of mount & use OS rsync

--- tools/onedrive_rsync.sh
+++ tools/onedrive_rsync.sh
@@ -16,12 +16,13 @@
 # Version history: 
 #   Aug 21, 2022 --- Original version
 #   Jan 11, 2024 --- refactor to use logger and function includes
+#   May 3,  2024 --- refactor to use rclone sync instead of mount & use OS rsync
 
 # version format YY.MM.DD
-version=24.01.11
+version=24.05.03
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-my_title="OneDrive rsync script for backup"
+my_title="OneDrive rsync script for backup using rclone."
 my_dirname=`dirname $0`
 my_path=$(cd $my_dirname; pwd -P)
 my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
@@ -29,16 +30,17 @@ default_scripts_github=$HOME/src/scripts.github
 scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
 # commandline options
-options_list="e:l:p:s:m:h"
+options_list="e:l:p:s:thv"
 
 # ensure path for cron runs
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
-
-# rsync/rclone options
-# note: -a option contains -l and -D so we use no-XXX to remove them as we don't need them
-rsync_opts="-azqt -O --no-links --no-D --delete --inplace --cvs-exclude --exclude=*.html --exclude=*.htm --exclude=*.backup --exclude=*.m3u --exclude=*.sh --exclude=thumb --exclude=jdothumb --exclude=*.exe --exclude=*.EXE --delete-excluded"
-rclone_opt="--vfs-cache-mode writes --vfs-cache-max-age 5m --vfs-cache-max-size 64M"
+# rclone options
+# note: --metadata is similar to -a i.e. copy file perm/time etc; -l copy links as link
+rclone_opts_quite="-q"
+rclone_opts_metadata="--metadata"
+rclone_opts="-l --exclude=*.htm --exclude=*.docx --exclude=*.backup --exclude=*.sh --exclude=*.html --exclude=*.htm --exclude=jdothumb/ --exclude=thumb/ --exclude=*.backup --exclude=*.m3u --exclude=*.sh"
+failure=0
 
 # backup locations
 photos_src="/var/www/photos"
@@ -49,94 +51,25 @@ debbie_backup="/data/debbie-backup"
 src_dirs="$photos_src $videos_src $scrapbooks_src $yt_videos $debbie_backup"
 
 onedrive_label="onedrive"
-mount_point="/mnt/onedrive"
 onedrive_root_path="/personal/home/media"
-onedrive_mounted=0
-onedrive_mount_wait=30
 
 usage() {
   cat << EOF
-$my_name - $my_title
+$my_title
 
 Usage: $my_name [options]
-  -s <source> ---> one or more source directories to backup [default: \"$src_dirs\"]"
-  -p <path>   ---> onedrive root path starting from label [$onedrive_label] to mount [default: $onedrive_root_path]"
-  -l <label>  ---> onedrive label from rclone.conf [default: $onedrive_label]"
-  -m <mount>  ---> local mount point to mount onedrive [default: $mount_point]"
-  -e <email>  ---> email address to send success/failure messages
-  -v          ---> enable verbose, otherwise just errors are printed
-  -h          ---> print usage/help
+  -s <source> ---> one or more source directories to backup [default: "$src_dirs"].
+  -p <path>   ---> onedrive root path starting from label [$onedrive_label] to mount [default: $onedrive_root_path].
+  -l <label>  ---> onedrive label from rclone.conf [default: $onedrive_label].
+  -e <email>  ---> email address to send success/failure messages.
+  -v          ---> enable verbose, otherwise just errors are printed.
+  -t          ---> test run just to see what would be done.
+  -h          ---> print usage/help.
 
-example: $my_name -h -v
+example: $my_name -s "/home/photos /home/videos" -e foo@bar.com
   
 EOF
   exit 0
-}
-
-is_mounted() {
-  if [ $os_name = "Darwin" ] ; then
-    if mount | grep "on $mount_point" > /dev/null; then
-      return 0
-    else
-      return 1
-    fi
-  else
-    mountpoint -q "$mount_point"
-    return $?
-  fi
-}
-
-check_onedrive() {
-  # make sure mount point exists
-  mkdir -p $mount_point
-  
-  # mount onedrive if not mounted already
-  is_mounted
-  if [ $? -ne 0 ]; then
-    log.stat "OneDrive is not mounted, attempting to mount..."
-
-    rclone $rclone_opt mount $onedrive_label:$onedrive_root_path $mount_point --daemon
-    rc=$?
-    if [ $rc -ne 0 ]; then
-      log.error "Error mounting OneDrive, exiting, error = $rc"
-      exit 1
-    fi
-
-    # wait few sec to check the drive mount again
-    log.stat "Waiting for $onedrive_mount_wait sec for OneDrive to mount..."
-    /bin/sync
-    sleep $onedrive_mount_wait 
-
-    is_mounted
-    if [ $? -ne 0 ]; then
-      log.error "Error unable to mount OneDrive... giving up!"
-      exit
-    fi
-    onedrive_mounted=1
-  fi
-  log.stat "OneDrive is mounted and ready..."
-}
-
-unmount_onedrive() {
-  log.stat "Leaving ondrive mounted since buffered data is not making to remote storage (need more research).." 
-  return
-
-  log.stat "Unmounting OneDrive..."
-  # just do a couple of syncs to flush buffers
-  sync
-  # unmount only if we mounted it in the first place
-  if [ $onedrive_mounted -eq 0 ]; then
-    log.warn "OneDrive was already mounted when we started, so leaving it mounted"
-    return
-  fi
-
-  fusermount -zu $mount_point
-  rc=$?
-  if [ $rc -eq 0 ]; then
-    log.stat "Onedrive unmount success"
-  else
-    log.error "Error unmounting onedrive, error = $rc"
-  fi 
 }
 
 # -------------------------------  main -------------------------------
@@ -165,11 +98,14 @@ while getopts "$options_list" opt; do
     p)
       onedrive_root_path=$OPTARG
       ;;
-    m)
-      mount_point=$OPTARG
+    t)
+      rclone_opts_quite="--dry-run"
       ;;
     s)
       src_dirs=$OPTARG
+      ;;
+    v)
+      verbose=1
       ;;
     ?|h|*)
       usage
@@ -177,24 +113,25 @@ while getopts "$options_list" opt; do
    esac
 done
 
-# check for onedrive availability
-check_onedrive
-
-# start rsync
-log.stat "Backup Sources: '$src_dirs'" 
-rsync $rsync_opts $src_dirs ${mount_point}/. >>$my_logfile 2>&1
-rc=$?
-if [ $rc -ne 0 ]; then
-  log.error "Error while rsync; error = $rc ... terminating."
-  unmount_onedrive
-  send_mail "1"
-  exit 1
+# rclone in MacOS does not support metadata flag!
+if [ "$os_name" = "Darwin" ] ; then
+  rclone_opts_metadata=""
 fi
 
-# unmount onedrive
-unmount_onedrive
+# start sync
+log.stat "Source dirs: '$src_dirs'"
+for src in $src_dirs ; do 
+  dir=`basename $src`
+  log.stat "Backing up: $src ..."
+  rclone sync $rclone_opts_quite $rclone_opts_metadata $rclone_opts $src ${onedrive_label}:${onedrive_root_path}/${dir} >> $my_logfile 2>&1
+  rc=$?
+  if [ $rc -ne 0 ]; then
+    log.error "Error while rclone sync; error = $rc, continuing w/ next source"
+    failure=1
+  fi
+done
 
 # mail and exit
 log.stat "OneDrive backup complete."
 log.stat "Total runtime: $(elapsed_time)"
-send_mail "0"
+send_mail "$failure"
