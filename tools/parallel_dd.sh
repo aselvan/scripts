a19f2c008f012f836a1636074881c25f55e60ac7
Arul Selvan <aselvan@selvans.net>
2024-06-12 09:56:25 -0500

    Added code to write tag/version file on root dir

--- tools/parallel_dd.sh
+++ tools/parallel_dd.sh
@@ -16,21 +16,24 @@
 #   May 12, 2024 --- Validate the output devices before using
 #   May 24, 2024 --- Fix GPT size mismatch, send mail optionally, -f to skip confirmation
 #   May 26, 2024 --- Added resize partition to fill, and extend NTFS to end of partition.
+#   Jun 12, 2024 --- Added option to write a tag/version file on new imaged disk
 #
 
 # version format YY.MM.DD
-version=24.05.26
+version=24.06.12
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
 my_title="Run dd in parallel to copy image to multiple devices."
 my_dirname=`dirname $0`
 my_path=$(cd $my_dirname; pwd -P)
 my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+my_name_noext="$(echo $my_name|cut -d. -f1)"
+
 default_scripts_github=$HOME/src/scripts.github
 scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
 # commandline options
-options="i:l:s:e:fxvh?"
+options="i:l:s:e:t:fxvh?"
 
 # ensure path for cron runs (prioritize usr/local first)
 export PATH="/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
@@ -41,20 +44,22 @@ dd_bs="32"
 failure=0
 skip_confirmation=0
 extend_ntfs=0
+tag=""
 
 usage() {
   cat << EOF
 $my_title
 
 Usage: $my_name [options]
-  -i <image> ---> The disk image to write to multiple devices 
-  -l <list>  ---> list of output devices write image
-  -s <size>  ---> buffer size (megabyte) argument for dd [default: $dd_bs]
-  -e <email> ---> email address to send success/failure emails
-  -f         ---> skip confirmation question for automated runs from cron.
-  -x         ---> extend the NTFS volume (assuming image is NTFS and last partion is NTFS) [default: NO]
-  -v         ---> enable verbose, otherwise just errors are printed
-  -h         ---> print usage/help
+  -i <image>  ---> The disk image to write to multiple devices 
+  -l <list>   ---> list of output devices write image
+  -s <size>   ---> buffer size (megabyte) argument for dd [default: $dd_bs]
+  -e <email>  ---> email address to send success/failure emails
+  -t <string> ---> A tag string to inlcude in /$my_name_noext.txt [default: no file written]
+  -f          ---> skip confirmation question for automated runs from cron.
+  -x          ---> extend the NTFS volume (assuming image is NTFS and last partion is NTFS) [default: NO]
+  -v          ---> enable verbose, otherwise just errors are printed
+  -h          ---> print usage/help
 
 example: $my_name -i myimage.iso -l "/dev/sdb /dev/sdb /dev/sdc" -e foo@bar.com
   
@@ -67,6 +72,45 @@ mail_and_exit() {
   log.stat "Exit Status: $failure"
   send_mail "$failure"
   exit $failure
+}
+
+write_tag_file() {
+  local dev=$1
+  if [ -z "$dev" ] ; then
+    log.error "Missing device!"
+    return
+  fi
+
+  # find last partition
+  local pnum=$(parted -s $dev print | awk '$1 ~ /^[0-9]+$/ { last = $1 } END { print last }')
+  if [ -z "$pnum" ]; then
+    log.error "Unable to find the last partition on $dev ... skiping tag file"
+    return
+  fi
+
+  # create a mount point in /tmp and mount the disk
+  mount_dir="/tmp/$my_name_noext"
+  mkdir $mount_dir
+
+  # mount the disk
+  log.stat "Mounting ${dev}${pnum} on $mount_dir ..."
+  mount ${dev}${pnum} $mount_dir
+  if [ $? -ne 0 ] ; then
+    log.error "Mount failed... skipping tag file ..."
+    return
+  fi
+
+  # write the tag file
+  log.stat "Writing tag/version file on root directory of ${dev}${pnum} ..."
+  cat << EOF > ${mount_dir}/{$my_name_noext}.txt
+  
+  Imaging Tool: $my_version
+  Image Tag:    $tag
+  Image Date:   `date`
+  
+EOF
+  # now unmount
+  umount $mount_dir
 }
 
 #
@@ -115,9 +159,6 @@ log.init $my_logfile
 # parse commandline options
 while getopts $options opt ; do
   case $opt in
-    e)
-      email_address=$OPTARG
-      ;;
     i)
       image_file="$OPTARG"
       ;;
@@ -126,6 +167,12 @@ while getopts $options opt ; do
       ;;
     s)
       dd_bs="$OPTARG"
+      ;;
+    e)
+      email_address=$OPTARG
+      ;;
+    t)
+      tag="$OPTARG"
       ;;
     f)
       skip_confirmation=1
@@ -201,8 +248,16 @@ done
 # if se need to extend the disk and NTFS filesystems
 if [ $extend_ntfs -ne 0 ] ; then
   for dev in $device_list ; do
-    log.stat "  Extend the partition and NTFS file system on: $dev"
+    log.stat "  Extend the partition and NTFS file system of: $dev"
     extend_ntfs_partition $dev
+  done
+fi
+
+# write tag/version file
+if [ ! -z "$tag" ] ; then
+  for dev in $device_list ; do
+    log.stat "  Writing tag file on for: $dev"
+    write_tag_file $dev
   done
 fi
 
