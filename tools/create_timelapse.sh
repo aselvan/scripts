8725ea6efb61fecdb20acf38129018f0cce5d803
Arul Selvan <aselvan@selvans.net>
2024-11-03 19:02:38 -0600

    Changed to use standard includes and magick instead of convert

--- tools/create_timelapse.sh
+++ tools/create_timelapse.sh
@@ -8,16 +8,26 @@
 #
 # Author:  Arul Selvan
 # Created: Aug 21, 2023
+# 
+# Version History:
+#   Aug 21, 2023 --- Original version
+#   Nov 3,  2024 --- Changed to use standard includes and magick instead of convert
 #
 
 # version format YY.MM.DD
-version=23.08.21
+version=24.11.03
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-dir_name=`dirname $0`
-my_path=$(cd $dir_name; pwd -P)
+my_title="Create a timelapse animated gif"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
-log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
+# commandline options
+options="i:o:l:d:f:s:vh?"
+
 default_list_file="/tmp/$(echo $my_name|cut -d. -f1).txt"
 list_file=""
 animation_output="/tmp/$(echo $my_name|cut -d. -f1).gif"
@@ -26,87 +36,45 @@ frame_delay="25"
 first_frame_delay=0
 loop=1
 
-log_init=0
-options="i:o:l:d:f:s:vh?"
-verbose=0
-failure=0
-green=32
-red=31
-blue=34
+# ensure path for cron runs (prioritize usr/local first)
+export PATH="/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
 
-# ensure path for cron runs
-export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
 usage() {
   cat << EOF
+$my_title
 
-  Usage: $my_name [options]
-     -i <listfile>   ---> input file contains list of images in the order to inlcude in animated gif
-     -o <outputfile> ---> output name for animated file [default: $animation_output]
-     -s <size>       ---> size in width/height [default: $size]
-     -l <loop>       ---> loop/iteration count [default: run once do not loop]
-     -d <delay>      ---> insert a delay (1/100 of sec) units [default: $frame_delay]
-     -f <delay>      ---> insert a different delay for first frame [default: same as -d option]
-     -v              ---> verbose mode prints info messages, otherwise just errors are printed 
-     -h              ---> print usage/help
+Usage: $my_name [options]
+  -i <listfile>   ---> file contains list of images (one per line) in the order
+  -o <outputfile> ---> output name for animated file [default: $animation_output]
+  -s <size>       ---> size in width/height [default: $size]
+  -l <loop>       ---> loop/iteration count [default: run once do not loop]
+  -d <delay>      ---> insert a delay (1/100 of sec) units [default: $frame_delay]
+  -f <delay>      ---> insert a different delay for first frame [default: same as -d option]
+  -v              ---> verbose mode prints info messages, otherwise just errors are printed 
+  -h              ---> print usage/help
 
-  example: $my_name -h
+  example: $my_name -o animated.gif -l10 -d100
   
 EOF
   exit 0
 }
 
-# -- Log functions ---
-log.init() {
-  if [ $log_init -eq 1 ] ; then
-    return
-  fi
 
-  log_init=1
-  if [ -f $log_file ] ; then
-    rm -f $log_file
-  fi
-  echo -e "\e[0;34m$my_version, `date +'%m/%d/%y %r'` \e[0m" | tee -a $log_file
-}
-
-log.info() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[0;32m$msg\e[0m" | tee -a $log_file 
-}
-log.debug() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[1;30m$msg\e[0m" | tee -a $log_file 
-}
-log.stat() {
-  log.init
-  local msg=$1
-  local color=$2
-  if [ -z $color ] ; then
-    color=$blue
-  fi
-  echo -e "\e[0;${color}m$msg\e[0m" | tee -a $log_file 
-}
-log.warn() {
-  log.init
-  local msg=$1
-  echo -e "\e[0;33m$msg\e[0m" | tee -a $log_file 
-}
-log.error() {
-  log.init
-  local msg=$1
-  echo -e "\e[0;31m$msg\e[0m" | tee -a $log_file 
-}
-
-# ----------  main --------------
-log.init
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  echo "See INSTALL instructions at: https://github.com/aselvan/scripts?tab=readme-ov-file#setup"
+  exit 1
+fi
+# init logs
+log.init $my_logfile
 
 # parse commandline options
 while getopts $options opt ; do
@@ -145,12 +113,13 @@ if [ -z $list_file ] ; then
 fi
 
 # run convert to create animation GIF
-convert -loop 1 -dispose previous -thumbnail $size -delay $frame_delay @$list_file ${animation_output}
+#magick -loop $loop -dispose previous -thumbnail $size -delay $frame_delay @$list_file ${animation_output}
+magick -loop $loop -dispose previous -delay $frame_delay -size $size @$list_file ${animation_output}
 
 # if different delay for 1st frame requested change the delay
 if [ $first_frame_delay -ne 0 ] ; then
   log.stat "  Reseting first frame delay to $first_frame_delay ..."
-  convert ${animation_output} \( -clone 0 -set delay $first_frame_delay \) -swap 0,-1 +delete ${animation_output}.tmp
+  magick ${animation_output} \( -clone 0 -set delay $first_frame_delay \) -swap 0,-1 +delete ${animation_output}.tmp
   mv ${animation_output}.tmp ${animation_output}
 fi
 log.stat "Output file is: $animation_output"
