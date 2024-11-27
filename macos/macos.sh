7a32f4a764d117ddc319ea4f0e87006535d28b8c
Arul Selvan <arul@selvansoft.com>
2024-11-26 21:04:14 -0600

    Moved network related functions to tools/network.sh script

--- macos/macos.sh
+++ macos/macos.sh
@@ -9,10 +9,11 @@
 # Version History:
 #   Aug 25, 2024 --- Original version
 #   Nov 11, 2024 --- Added showipexternal command, show interface on showip command
+#   Nov 26, 2024 --- Moved all network functions related to tools/network.sh script
 #
 
 # version format YY.MM.DD
-version=2024.10.11
+version=2024.10.26
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
 my_title="Misl tools for macOS all in one place"
@@ -24,13 +25,10 @@ scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 arp_entries="/tmp/$(echo $my_name|cut -d. -f1)_arp.txt"
 
 # commandline options
-options="c:i:n:avh?"
+options="c:vh?"
 
 command_name=""
-supported_commands="ip|lanip|wanip|mac|dhcp|scannetwork|mem|vmstat|cpu"
-iface=""
-my_net="192.168.1.0/24"
-my_ip=""
+supported_commands="mem|vmstat|cpu"
 
 # ensure path for cron runs (prioritize usr/local first)
 export PATH="/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
@@ -41,55 +39,15 @@ $my_name --- $my_title
 
 Usage: $my_name [options]
   -c <command>   ---> command to run [see supported commands below]
-  -i <interface> ---> network interface to use [Default: $iface]
-  -n <network>   ---> CIDR address to scan for 'shownetwork' command [Default: $my_net]
   -v             ---> enable verbose, otherwise just errors are printed
   -h             ---> print usage/help
 
 Supported commands: $supported_commands
-example: $my_name -c ipexternal
+example: $my_name -c mem
 
   
 EOF
   exit 0
-}
-
-# detect active, IP assigned interface, first one matchign will be used
-function get_interface() {
-  local ipaddr
-  for iface in `ipconfig getiflist` ; do
-    ipaddr=`ipconfig getifaddr $iface`
-    if [ ! -z "$ipaddr" ] ; then
-      log.debug "Using interface: $iface"
-      return
-    fi
-  done
-  iface="en0"
-  log.debug "Using interface: $iface"
-}
-
-function showmac() {
-  mac_addr=`ifconfig $iface | grep ether| awk '{print $2;}'`
-  log.stat "\tMAC address of $iface is: $mac_addr" $green
-}
-
-function showdhcp() {
-  log.stat "`ipconfig getpacket $iface`"
-}
-
-function scannetwork() {
-  nmap --host-timeout 10 -T5 $my_net >/dev/null 2>&1
-  arp -an|egrep -v 'incomplete|ff:ff:ff:ff|169.254|224.|239.|.255)'> $arp_entries
-
-  cat $arp_entries | while read -r line ; do
-    ip=$(echo $line|awk -F '[()]|at | on ' '{print $2}')
-    mac=$(echo $line|awk -F '[()]|at | on ' '{print $4}')
-    host=`dig +short +timeout=1 +retry=0 +nostats -x $ip|sed -e 's/\.$//'`
-    if [[ $host == "" || $host == *"communications error"* || $host == *"connection timed out"* ]] ; then
-      host="N/A"
-    fi
-    log.stat "  $ip\t$host\t # macaddress: $mac" $green
-  done
 }
 
 showmem() {
@@ -110,7 +68,7 @@ showcpu() {
   log.stat "\tBrand:  `sysctl -a machdep.cpu.brand_string|awk -F: '{print $2}'`" $green
   log.stat "\tFamily: `sysctl -a machdep.cpu.extfamily|awk -F: '{print $2}'`" $green
   log.stat "\tModel:  `sysctl -a machdep.cpu.model|awk -F: '{print $2}'`" $green
-  log.stat "\t#cores: `sysctl -a machdep.cpu.core_count|awk -F: '{print $2}'`" $green
+  log.stat "\tCores:  `sysctl -a machdep.cpu.core_count|awk -F: '{print $2}'`" $green
 }
 
 
@@ -156,35 +114,7 @@ if [ -z "$command_name" ] ; then
   usage
 fi
 
-# determine the interface if one not provided.
-if [ -z "$iface" ] ; then
-  get_interface
-fi
-
-my_ip=`ipconfig getifaddr $iface`
-my_net=`echo $my_ip |awk -F. '{print $1"."$2"."$3".0/24"; }'` 
-
 case $command_name in 
-  mac)
-    showmac
-    ;;
-  ip)
-    log.stat "\tLAN IP: $my_ip on interface: $iface"
-    log.stat "\tWAN IP: `curl -s ifconfig.me`"
-    ;;
-  lanip)
-    log.stat "\tLAN IP: $my_ip on interface: $iface"
-    ;;
-  wanip)
-    log.stat "\tWAN IP: `curl -s ifconfig.me`"
-    ;;
-  dhcp)
-    showdhcp
-    ;;
-  scannetwork)
-    log.stat "Scanning network $my_net using interface $iface ... [note: make sure $iface is correct]"
-    scannetwork
-    ;;
   mem)
     showmem
     ;;
