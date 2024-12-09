63c9e2a928e2072a7ce67f33ceaf0ddfa53449cb
Arul Selvan <aselvan@selvans.net>
2024-12-09 13:23:38 -0600

    Added more functions consolidated from bashrc

--- tools/exif.sh
+++ tools/exif.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# exif_check.sh --- Query specific exif metadata present on a media file
+# exif_check.sh --- Wrapper over exiftool to get various metadata.
 #
 # pre-req: exiftool
 # install: 
@@ -12,14 +12,14 @@
 #
 # Version History:
 #   Jan 13, 2023 --- Original version
+#   Dec 9,  2024 --- Added more functions consolidated from bashrc
 #
 
-
 # version format YY.MM.DD
-version=24.04.25
+version=24.12.09
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-my_title="Query specific exif metadata present on a media file."
+my_title="Wrapper over exiftool to get various metadata."
 my_dirname=`dirname $0`
 my_path=$(cd $my_dirname; pwd -P)
 my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
@@ -27,61 +27,33 @@ default_scripts_github=$HOME/src/scripts.github
 scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
 # commandline options
-options="p:ltah"
+options="p:dlczvh"
+
+format_file="/tmp/$(echo $my_name|cut -d. -f1).fmt"
+format_file_string=""
+source_path=""
+zap_metadata=0
 
 # ensure path for cron runs (prioritize usr/local first)
 export PATH="/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
-
-source_path=""
-exiftool_opt="-m"
-check_gps=0
-check_camera=0
-check_createtime=0
-dir_name=""
 
 usage() {
   cat << EOF
 $my_name --- $my_title
 
 Usage: $my_name [options]
-  -l  ---> check if lat/lon present as metadata
-  -t  ---> check if createdate present as metadata
-  -a  ---> check both createdate and gps present as metadata
-  -v  ---> verbose mode prints info messages, otherwise just errors are printed
-  -h  ---> print usage/help
-  
-example: $my_name -l -p image.jpg
+  -p <path>  ---> Directory path or file or wildcard
+  -l         ---> print lat/lon
+  -d         ---> print date
+  -c         ---> print camera model
+  -z         ---> zap/clear all metadata [Note: all other options are ignored if option enabled]
+  -v         ---> verbose mode prints info messages, otherwise just errors are printed
+  -h         ---> print usage/help
+
+example: $my_name -p /path/*.jpg -g -d
+
 EOF
   exit 0
-}
-
-has_camera() {
-  local f=$1
-  # must be an easier way to tell exiftool to spit out tag value but this should work as well
-  camera_model=$(exiftool $f | grep "Camera Model Name"|awk -F: '{print $2}')
-  if [ -z "$camera_model" ] ; then
-    camera_model="Unknown"
-  fi
-}
-
-has_gps() {
-  local f=$1
-  exiftool $exiftool_opt -if 'defined $gpslatitude' -filename -T $f 2>&1 >/dev/null
-  if [ $? -eq 0 ] ; then
-    gps_present="Yes"
-  else
-    gps_present="No"
-  fi
-}
-
-has_createtime() {
-  local f=$1
-  exiftool $exiftool_opt -if 'defined $createdate' -filename -T $f 2>&1 >/dev/null
-  if [ $? -eq 0 ] ; then
-    createtime_present="Yes"
-  else
-    createtime_present="No"
-  fi
 }
 
 # -------------------------------  main -------------------------------
@@ -99,26 +71,27 @@ fi
 # init logs
 log.init $my_logfile
 
+# check required tools
+check_installed exiftool
 
+format_file_string="\$FileName"
 # parse commandline options
 while getopts $options opt; do
   case $opt in
     p)
       source_path="$OPTARG"
       ;;
-    l)
-      check_gps=1
+    d)
+      format_file_string="$format_file_string , Date: \$DateTimeOriginal"
       ;;
-    t)
-      check_createtime=1
+    l)
+      format_file_string="$format_file_string , Lat: \$gpslatitude, Lon: \$gpslongitude"
       ;;
     c)
-      check_camera=1
+      format_file_string="$format_file_string , Camera: \$model"
       ;;
-    a)
-      check_gps=1
-      check_createtime=1
-      check_camera=1      
+    z)
+      zap_metadata=1
       ;;
     v)
       verbose=1
@@ -134,38 +107,14 @@ if [ -z "$source_path" ] ; then
   usage
 fi
 
-# check if source path is a single file
-if [ -f "$source_path" ] ; then
-  file_list="$source_path"
+
+# check if this run is to clear/zap metadata, if so do it and exit
+if [ $zap_metadata -eq 1 ] ; then
+  log.stat "Clearing all medatadata ..." $red
+  exiftool -quiet -f -m -all= -overwrite_original $source_path
 else
-  dir_name=$(dirname "$source_path")
-  file_name=$(basename "$source_path")
-  file_list=`ls -1 $dir_name/$file_name`
+  echo $format_file_string > $format_file
+  # run exiftool with our desired format.
+  log.stat "`exiftool -quiet -f -p $format_file $source_path`" $green
 fi
 
-for fname in ${file_list} ;  do
-  if [ ! -z "$dir_name" ] ; then
-    fname="$dir_name/$file_name/$fname"
-  fi
-
-  is_media $fname
-  if [ $? -ne 0 ] ; then
-    log.warn "The file '$fname' is not known media type, skipping ..."
-    continue
-  fi
-  output="File Name: `basename $fname`"
-  if [ $check_gps -eq 1 ] ; then
-    has_gps $fname
-    output="$output ; GPS:$gps_present"
-  fi
-  if [ $check_createtime -eq 1 ] ; then
-    has_createtime $fname
-    output="$output ; Create Time:$createtime_present"
-  fi
-  if [ $check_camera -eq 1 ] ; then
-    has_camera $fname
-    output="$output ; Camera:$camera_model"
-  fi
-
-  log.stat "$output"
-done
