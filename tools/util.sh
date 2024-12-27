7dfd5bcb806cd1a7d677c76949490a684ea7c86c
Arul Selvan <arul@selvansoft.com>
2024-12-27 12:25:57 -0600

    Added knock function

--- tools/util.sh
+++ tools/util.sh
@@ -9,10 +9,11 @@
 #   Jan 10, 2012 --- Original version (moved from .bashrc)
 #   Dec 23, 2024 --- moved more from .bashrc
 #   Dec 26, 2024 --- Added txt2mp3
+#   Dec 27, 2024 --- Added knock
 #
 
 # version format YY.MM.DD
-version=2024.12.26
+version=2024.12.27
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
 my_title="Misl util tools wrapper all in one place"
@@ -24,10 +25,10 @@ scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 arp_entries="/tmp/$(echo $my_name|cut -d. -f1)_arp.txt"
 
 # commandline options
-options="c:n:i:o:a:q:d:vh?"
+options="c:n:i:o:a:q:d:s:vh?"
 
 command_name=""
-supported_commands="tohex|todec|toascii|calc|rsync|compresspdf|dos2unix|tx2mp3|vid2gif"
+supported_commands="tohex|todec|toascii|calc|rsync|knock|compresspdf|dos2unix|tx2mp3|vid2gif"
 number=""
 ifile=""
 ofile=""
@@ -35,7 +36,8 @@ args=""
 rsync_log_file="/tmp/$(echo $my_name|cut -d. -f1)_rsync.log"
 rsync_opts="-rlptgoq --ignore-errors --no-specials --no-devices --delete-after --cvs-exclude --log-file=$rsync_log_file --temp-dir=/tmp --exclude=\"*.vmdk\""
 pdf_quality="/ebook"
-delay=3 # used for vid2gif
+delay=3 # for vid2gif
+host_port="" # for knock
 
 # ensure path for cron runs (prioritize usr/local first)
 export PATH="/usr/local/bin:/usr/bin:/usr/sbin:/bin:/sbin:$PATH"
@@ -51,6 +53,7 @@ Usage: $my_name [options]
   -o <file/path> ---> output for commands require output argument like rsync
   -a <arg>       ---> for commands like 'calc'
   -d <delay>     ---> frame delay used for vid2gif [Default: $delay]
+  -s <host:port> ---> for 'knock'; need hostname and port knock open using fwknop client
   -q <quality>   ---> for 'compresspdf'; valid entries are "/printer|/ebook|/screen" [Default: $pdf_quality]
   -v             ---> enable verbose, otherwise just errors are printed
   -h             ---> print usage/help
@@ -125,7 +128,6 @@ function do_compresspdf() {
 
 }
 
-
 function do_dos2unix() {
   if [ -z $ifile ] ; then
     log.error "dos2unix needs input file, see usage"
@@ -169,6 +171,24 @@ function do_vid2gif() {
   ffmpeg -i $ifile -pix_fmt rgb24 -r 15 -f gif - 2>/dev/null | gifsicle --optimize=3 --delay=$delay 2>/dev/null > $ofile 
 }
 
+function do_knock() {
+  check_installed fwknop
+
+  if [ -z $host_port ] ; then
+    log.error "Need host:port for do_knock function, see usage"
+    usage
+  fi
+  local host="${host_port%%:*}"
+  local port="${host_port##*:}"
+  if [ "$port" == "$host" ] ; then
+    port=1863
+    log.stat "\tPort not specified, using $port ..."
+  fi
+  # determine our egress (i.e. -R option on fwknop is not working so doing it ourself)
+  local myip=`curl -s https://ifconfig.me`
+  log.stat "\tRunning knock sequence on $host to open port# $port for $myip ..."
+  fwknop -A tcp/$port -a $myip -D $host
+}
 
 
 # -------------------------------  main -------------------------------
@@ -209,6 +229,9 @@ while getopts $options opt ; do
       ;;
     q)
       pdf_quality="$OPTARG"
+      ;;
+    s)
+      host_port="$OPTARG"
       ;;
     v)
       verbose=1
@@ -253,6 +276,9 @@ case $command_name in
     ;;
   vid2gif)
     do_vid2gif
+    ;;
+  knock)
+    do_knock
     ;;
   *)
     log.error "Invalid command: $command_name"
