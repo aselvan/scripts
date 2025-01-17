26964ac3f8a75aab8df2b326c1f9b5548689bf25
Arul Selvan <aselvan@selvans.net>
2025-01-17 09:31:08 -0600

    Added additional check at ProjectHoneypot.org

--- security/ismalicious.sh
+++ security/ismalicious.sh
@@ -1,38 +1,47 @@
 #!/usr/bin/env bash
 #
-# ismalicious.sh --- wrapper over ismalicious.com API 
+# ismalicious.sh --- Query ismalicious.com API and/or Project Honeypot API
 #
 # Check if an IP/Domain's reputation, vulnerabilities, geolocaion or whois info using the
-# ismalicious.com free API and prints the JSON output
+# ismalicious.com free API and/or projecthoneypot.org, and prints the JSON output.
 # 
-# Note: To use this script, create a free acount account and get API key and place it
-# in your $HOME/.ismalicious.com-apikey.txt  or pass it via commandline
+# Note: To use this script, create a free acount account on ismalicious.com and/or
+#   projecthoneyport.org and place them in your home directory at filename shown below
+#
+#   $HOME/.ismalicious.com-apikey.txt      --- ismalicious.com API key
+#   $HOME/.projecthoneypot.org-apikey.txt  --- projecthoneypot.org access key
 #
 # Author:  Arul Selvan
 # Created: Jan 14, 2025
 #
 # Version History:
 #   Jan 14,  2025 --- Orginal version
+#   Jan 17,  2025 --- Added additional check at ProjectHoneypot.org
 #
 
 # version format YY.MM.DD
-version=25.01.14
+version=25.01.17
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-my_title="Query using ismalicious.com API"
+my_title="Query ismalicious.com API and/or Project Honeypot API"
 my_dirname=`dirname $0`
 my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
 default_scripts_github=$HOME/src/scripts.github
 scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
 # commandline options
-options="c:n:k:vh?"
+options="c:n:k:K:vh?"
 
-api_url_base="https://ismalicious.com/api/check"
-api_key_file="$HOME/.ismalicious.com-apikey.txt"
 supported_commands="reputation|vulnerabilities|geolocation|whois"
+ismalicious_api_url_base="https://ismalicious.com/api/check"
+projecthoneypart_dns_suffix="dnsbl.httpbl.org"
+
+ismalicious_api_key_file="$HOME/.ismalicious.com-apikey.txt"
+projecthoneypot_api_key_file="$HOME/.projecthoneypot.org-apikey.txt"
+ismalicious_api_key=
+projecthoneypot_api_key=
+
 command_name="reputation"
-api_key=""
 name=""
 http_output="/tmp/$(echo $my_name|cut -d. -f1).txt"
 
@@ -44,16 +53,76 @@ $my_title
 Usage: $my_name [options]
   -c <command>  ---> command to run [Default: $command_name]. See supported commands below
   -n <name>     ---> Domain/IP name to check
-  -k <apikey>   ---> ismalicious.com API key [Default: read from $api_key_file]
+  -k <apikey>   ---> ismalicious.com API key [Default: read from $ismalicious_api_key_file]
+  -K <apikey>   ---> ProjectHoneypot access key [Default: read from $projecthoneypot_api_key_file]
   -v            ---> enable verbose, otherwise just errors are printed
   -h            ---> print usage/help
 
 Supported commands: $supported_commands  
 example: $my_name -n 5.167.71.233
-example: $my_name -c whois -n qouv.fr 
+example: $my_name -c whois -n qouv.fr
  
 EOF
   exit 0
+}
+
+# check using ismalicious API
+check_ismalicious() {
+  log.stat "Checking $command_name of $name using ismalicious API ..."
+  http_status=$(curl -s -o $http_output -w "%{http_code}" -H "X-API-KEY: $ismalicious_api_key" ${ismalicious_api_url_base}/${command_name}?query=$name)
+  log.debug "HTTP status: $http_status"
+  if [ "$http_status" -eq 200 ] ; then
+    check_installed jq noexit
+    if [ $? -eq 0 ] ; then
+      cat $http_output | jq
+    else
+      cat $http_output
+    fi
+  else
+    log.error "\tAPI call failed! HTTP status = $http_status"
+  fi
+}
+
+# decode the response
+# See documentation at https://www.projecthoneypot.org/httpbl_api.php
+#
+# example decode of a return response: 127.1.55.1 
+#   127 - always 127. If it is not 127, there was an error
+#   1   - number of days last activity (0-255). Lower is most recent
+#   55  - threat score (0-255). Higher is bad, 0 is no threat.
+#   1   - type (0=searchengine; 1=suspicious, 2=harvester, 4=comment_spammer, rest are future use)
+decode_projecthoneypot_response() {
+  local resp=$1
+  IFS='.' read -r -a resp_array <<< "$resp"
+  if [ ${resp_array[0]} -ne 127 ] ; then
+    log.error "\tERROR: response is not ${resp_array[2]} is not expected 127"
+    return
+  fi
+  log.stat "\tMalicious:    YES [seen as recently as of last ${resp_array[1]} day(s)]." $red
+  log.stat "\tThreat score: ${resp_array[2]}/255. [Note: score of 0 is clean]" $red
+  log.stat "\tThreat type:  ${resp_array[3]} [note: 0=searchengine; 1=suspicious, 2=harvester, 4=comment_spammer]" $red
+}
+
+check_projecthoneypot() {
+  # if name is not IP just ignore
+  log.debug "Validating $name for IP address ..."
+  validate_ip $name
+  if [ $? -ne 0 ] ; then
+    return
+  fi
+
+  log.stat "Checking $command_name of $name using ProjectHoneypot API ..." 
+  # reverse IP to make DNS query with projecthonepot 
+  rev_ip=$(reverse_ip $name)
+  log.debug "Reversed IP: $rev_ip"
+
+  dns_resp=`dig +short ${projecthoneypot_api_key}.$rev_ip.${projecthoneypart_dns_suffix}`
+  log.debug "Project Honeypot response: $dns_resp"
+  if [ "$dns_resp" != "" ] ; then
+    decode_projecthoneypot_response $dns_resp
+  else
+    log.warn "\tEmpty response ProjectHoneypot API, likely no entry for this IP."
+  fi
 }
 
 
@@ -72,10 +141,12 @@ fi
 # init logs
 log.init $my_logfile
 
-
-# read default api key
-if [ -f $api_key_file ] ; then
-  api_key=`cat $api_key_file`
+# read API and access keys for ismalicious.com and projecthoneypot.org
+if [ -f $ismalicious_api_key_file ] ; then
+  ismalicious_api_key=`cat $ismalicious_api_key_file`
+fi
+if [ -f $projecthoneypot_api_key_file ] ; then
+  projecthoneypot_api_key=`cat $projecthoneypot_api_key_file`
 fi
 
 # parse commandline options
@@ -88,7 +159,10 @@ while getopts $options opt ; do
       name="$OPTARG"
       ;;
     k)
-      api_key="$OPTARG"
+      ismalicious_api_key="$OPTARG"
+      ;;
+    K)
+      projecthoneypot_api_key="$OPTARG"
       ;;
     v)
       verbose=1
@@ -98,6 +172,7 @@ while getopts $options opt ; do
       ;;
   esac
 done
+
 
 # check and validate valid command
 if [[ "|$supported_commands|" != *"|$command_name|"* ]] ; then
@@ -111,20 +186,16 @@ if [ -z "$name" ] ; then
   usage
 fi
 
-if [ -z "$api_key" ] ; then
-  log.error "Missing API key, see usage below"
+if [ -z "$ismalicious_api_key" ] || [ -z $projecthoneypot_api_key ] ; then
+  log.error "Missing either (ismalicious.com or projecthoneyport.org) keys, see usage below"
   usage
 fi
 
-log.stat "$my_title for $command_name of $name ..."
-http_status=$(curl -s -o $http_output -w "%{http_code}" -H "X-API-KEY: $api_key" ${api_url_base}/${command_name}?query=$name)
-if [ "$http_status" -eq 200 ] ; then
-  check_installed jq noexit
-  if [ $? -eq 0 ] ; then
-    cat $http_output | jq
-  else
-    cat $http_output
-  fi
-else
-  log.error "API call failed! HTTP status = $http_status"
+# first check ismalicious.com
+check_ismalicious
+
+# if the command is "reputation" check ProjectHoneyPot as well
+if [ "$command_name" == "reputation" ] ; then
+  check_projecthoneypot
 fi
+
