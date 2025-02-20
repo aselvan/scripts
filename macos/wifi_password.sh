4562b98ba031ec03a3a45e76540dc06b11567000
Arul Selvan <aselvan@selvans.net>
2025-02-20 13:10:58 -0600

    Use standard includes, remove depricated airport command

--- macos/wifi_password.sh
+++ macos/wifi_password.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
+################################################################################
 #
-# wifi_password.sh --- handy script to read the wifi password from MacOS keychain
+# wifi_password.sh --- Read the wifi password from MacOS keychain
 #
 #
 # Author:  Arul Selvan
 # Created: Jul 28, 2023
 #
+################################################################################
+# Version History:
+#   July 28, 2023 --- Original version
+#   Feb  20, 2025 --- Use standard includes, remove depricated airport command
+# 
+################################################################################
 
 # version format YY.MM.DD
-version=23.07.28
+version=25.02.20
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-os_name=`uname -s`
-dir_name=`dirname $0`
-my_path=$(cd $dir_name; pwd -P)
+my_title="Read the wifi password from MacOS keychain"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
-log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
-log_init=0
-
+# commandline options
 options="s:lvh?"
-verbose=0
 ssid=""
 airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 
@@ -28,63 +35,19 @@ export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
 usage() {
   cat << EOF
+$my_name --- $my_title
 
-  Usage: $my_name [options]
-     -s <ssid>  ---> Your wifi ssid whose password you need to read
-     -l         ---> List all the wifi SSIDs saved in your OS.
-     -v         ---> verbose mode prints info messages, otherwise just errors are printed
-     -h         ---> print usage/help
+Usage: $my_name [options]
+  -s <ssid>  ---> Your wifi SSID whose password you need to read 
+  -l         ---> List all the wifi SSIDs saved in your WiFi preferences
+  -v         ---> verbose mode prints info messages, otherwise just errors are printed
+  -h         ---> print usage/help
 
-  example: $my_name -s MY_SSID
+example:
+  $my_name -s MY_SSID
   
 EOF
   exit 0
-}
-
-log.init() {
-  if [ $log_init -eq 1 ] ; then
-    return
-  fi
-
-  log_init=1
-  if [ -f $log_file ] ; then
-    rm -f $log_file
-  fi
-  echo -e "\e[0;34m$my_version, `date +'%m/%d/%y %r'` \e[0m" | tee -a $log_file
-}
-
-log.info() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[0;32m$msg\e[0m" | tee -a $log_file 
-}
-log.debug() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[1;30m$msg\e[0m" | tee -a $log_file 
-}
-
-log.stat() {
-  log.init
-  local msg=$1
-  echo -e "\e[0;34m$msg\e[0m" | tee -a $log_file 
-}
-
-log.warn() {
-  log.init
-  local msg=$1
-  echo -e "\e[1;33m$msg\e[0m" | tee -a $log_file 
-}
-log.error() {
-  log.init
-  local msg=$1
-  echo -e "\e[1;31m$msg\e[0m" | tee -a $log_file 
 }
 
 list_ssids() {
@@ -94,32 +57,42 @@ list_ssids() {
     log.error "Not able to determine your WiFi interface!"
     exit 2
   fi
-  log.info "Your WiFi interface is '$wifi_dev'"
-  log.info "Below is a list of WiFi SSIDs saved."
-
+  log.stat "List of SSIDs for your Wi-Fi interface: '$wifi_dev'"
   networksetup -listpreferredwirelessnetworks $wifi_dev
   exit 0
 }
 
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  echo "See INSTALL instructions at: https://github.com/aselvan/scripts?tab=readme-ov-file#setup"
+  exit 1
+fi
+# init logs
+log.init $my_logfile
 
-# ----------  main --------------
-log.init
 if [ "$os_name" != "Darwin" ] ; then
   log.error "error" "This script is for MacOS only!"
-  exit 1
+  exit 0
 fi
 
 # parse commandline options
 while getopts $options opt ; do
   case $opt in
-    v)
-      verbose=1
-      ;;
     s)
       ssid="$OPTARG"
       ;;
     l)
       list_ssids
+      ;;
+    v)
+      verbose=1
       ;;
     ?|h|*)
       usage
@@ -129,18 +102,18 @@ done
 
 if [ -z $ssid ] ; then
   log.stat "SSID not provided, attempting to determine currently connected SSID ..."
-  ssid=`$airport -I |awk '/ SSID:/ {print $2}'`
+  ssid=`system_profiler SPAirPortDataType | awk '/Current Network Information/{getline; print $1; exit}'|tr -d ':'`
   if [ -z "$ssid" ] ; then
     log.error "Unable to read current SSID!"
     exit 1
   fi
-  log.info "Using SSID '$ssid'..."
+  log.stat "Using SSID: '$ssid'..."
 fi
 
-log.stat "Enter your MacOS username and password when prompted to continue ..."
+log.stat "Enter your MacOS username and password when prompted ..."
 password=$(security find-generic-password -ga $ssid 2>&1 >/dev/null|awk '/password:/ {print $2;}'|tr -d '"')
-log.stat "SSID=$ssid  password=$password"
+log.stat "SSID ($ssid) password: $password"
 
 # copy password to paste buffer
 echo $password|pbcopy
-log.stat "Password is copied to paste buffer for convenience [Cmd+v to paste]"
+log.stat "Password is also copied to paste buffer for convenience [cmd+v to paste]"
