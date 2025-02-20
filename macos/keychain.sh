67a0a258bbca3d95c51847a58dd16d27a93ddd1f
Arul Selvan <aselvan@selvans.net>
2025-02-20 11:15:29 -0600

    Use standard includes and misl changes

--- macos/keychain.sh
+++ macos/keychain.sh
@@ -1,31 +1,41 @@
 #!/usr/bin/env bash
+################################################################################
 #
-# keychain.sh --- store and retrive passwords in native MacOS keychain
+# keychain.sh --- Store/Retrive passwords in native MacOS keychain
 #
 # This script creates a separate keychain to add/delete/view passwords. This
 # can be used to store any thing leveraging Apple secret store, comes handy 
 # to store stuff that is not available to mac applications. The keystore file
 # will be in $HOME/Library/Keychains/$USER.keychain-db. The Apple keychain 
-# app is unaware so you have to add (open) this file once to see it in the 
-# keychain app which is not necessary or needed for this script to work.
+# app is unaware so you have to add (i.e. open) this file once to see it in 
+# the keychain app which is not necessary or needed for this script to work.
 #
 # Note: by default keychain file will be named after your username ($USER)
 #
 # Author:  Arul Selvan
 # Created: Jul 30, 2022
 #
+################################################################################
+# Version History:
+#   July 30, 2022 --- Original version
+#   Feb  20, 2025 --- Use standard includes and misl changes
+# 
+################################################################################
 
 # version format YY.MM.DD
-version=23.07.30
+version=25.02.20
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-host_name=`hostname`
-os_name=`uname -s`
+my_title="Store/Retrive passwords in native MacOS keychain"
+my_dirname=`dirname $0`
+my_path=$(cd $my_dirname; pwd -P)
+my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
+default_scripts_github=$HOME/src/scripts.github
+scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
-log_file="/tmp/$(echo $my_name|cut -d. -f1).log"
-log_init=0
-options="k:a:w:u:p:c:vh?"
-verbose=0
+# commandline options
+options="k:c:w:u:p:t:vh?"
+
 keychain_file_name="${USER}"
 keychain_file_path="${HOME}/Library/Keychains/${keychain_file_name}.keychain-db"
 website=""
@@ -33,96 +43,101 @@ username=""
 password=""
 action=""
 comment="Password added by $my_version"
+command_name=""
+supported_commands="add|show|delete|showall"
+
 # ensure path for cron runs
 export PATH="/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin:$PATH"
 
 usage() {
   cat << EOF
+$my_name --- $my_title
 
-  Usage: $my_name [options]
-     -k <keychain> ---> name to create a separate keychain store [default: $keychain_file_name]
-     -a <action>   ---> Action is one of "add" or "delete" "show"
-     -w <website>  ---> website name for which your user/password applies [example: yahoo.com]
-     -u <username  ---> The username to store
-     -p <password> ---> The password to store
-     -c <comment>  ---> Quoted comment string [example: "account details for yahoo.com"]
-     -v            ---> verbose mode prints info messages, otherwise just errors are printed
-     -h            ---> print usage/help
+Usage: $my_name [options]
+  -k <keychain> ---> name to create a separate keychain store [default: $keychain_file_name]
+  -c <command>  ---> command to run [see supported commands below] 
+  -w <website>  ---> website name for which your user/password applies [example: yahoo.com]
+  -u <username  ---> The username to store [note: you can have multiple usernames for same website]
+  -p <password> ---> The password to store
+  -t <message>  ---> a quoted string [example: "username/password for website yahoo.com"]
+  -v            ---> enable verbose, otherwise just errors are printed
+  -h            ---> print usage/help
 
-  example: $my_name -a add -w yahoo.com -u foobar -p password123
-  example: $my_name -a show -w yahoo.com -u foobar
-  example: $my_name -a delete -w yahoo.com -u foobar
+Supported commands: $supported_commands
+
+example(s): 
+  $my_name -c add -w yahoo.com -u foobar -p password123 -t "Yahoo password entry"
+  $my_name -c show -w yahoo.com -u foobar
+  $my_name -c delete -w yahoo.com -u foobar
   
 EOF
   exit 0
 }
 
-log.init() {
-  if [ $log_init -eq 1 ] ; then
-    return
-  fi
-
-  log_init=1
-  if [ -f $log_file ] ; then
-    rm -f $log_file
-  fi
-  echo -e "\e[0;34m$my_version, `date +'%m/%d/%y %r'` \e[0m" | tee -a $log_file
-}
-
-log.info() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[0;32m$msg\e[0m" | tee -a $log_file 
-}
-log.debug() {
-  if [ $verbose -eq 0 ] ; then
-    return;
-  fi
-  log.init
-  local msg=$1
-  echo -e "\e[1;30m$msg\e[0m" | tee -a $log_file 
-}
-
-log.stat() {
-  log.init
-  local msg=$1
-  echo -e "\e[0;34m$msg\e[0m" | tee -a $log_file 
-}
-
-log.warn() {
-  log.init
-  local msg=$1
-  echo -e "\e[1;33m$msg\e[0m" | tee -a $log_file 
-}
-log.error() {
-  log.init
-  local msg=$1
-  echo -e "\e[1;31m$msg\e[0m" | tee -a $log_file 
-}
-
 check_args() {
-  if [ -z $action ] ; then
-    log.error "missing required argument 'action'! See usage"
+  if [ -z $command_name ] ; then
+    log.error "missing required argument i.e. command. See usage below"
     usage
   fi
   if [ -z $website ] ; then
-    log.error "missing required argument 'website'! See usage"
+    log.error "missing required argument! i.e. website. See usage below"
     usage
   fi
   if [ -z $username ] ; then
-    log.error "missing required argument 'username'! See usage"
+    log.error "missing required argument! i.e. username. See usage below"
     usage
   fi
 }
 
-# ----------  main --------------
-log.init
+do_add() {
+  if [ -z $password ] ; then
+    log.error "missing required argument! i.e. password. See usage below"
+    usage
+  fi
+  log.stat "Adding password for website/username: $website/$username"
+  security add-generic-password -j "$comment" -s $website -a $username -w $password $keychain_file_name.keychain
+  if [ $? -ne 0 ] ; then
+    log.error "Error adding $wesite/$username key!"
+  else
+    log.stat "The specifed entry is added"
+  fi
+
+}
+
+do_delete() {
+  log.stat "Deleting entry for website/username: $website/$username"
+  security delete-generic-password -s $website -a $username $keychain_file_name.keychain 2<&1 >>$my_logfile
+  if [ $? -ne 0 ] ; then
+    log.error "Error deleting $website/$username key. Are you sure the key is correct?"
+  fi
+}
+
+do_show() {
+  log.stat "Password for website/username: $website/$username is below ..."
+  security find-generic-password -s $website -a $username -w $keychain_file_name.keychain
+  if [ $? -ne 0 ] ; then
+    log.error "The key $wesite/$username not found in keychain, check and try again"
+  fi
+}
+
+# -------------------------------  main -------------------------------
+# First, make sure scripts root path is set, we need it to include files
+if [ ! -z "$scripts_github" ] && [ -d $scripts_github ] ; then
+  # include logger, functions etc as needed 
+  source $scripts_github/utils/logger.sh
+  source $scripts_github/utils/functions.sh
+else
+  echo "SCRIPTS_GITHUB env variable is either not set or has invalid path!"
+  echo "The env variable should point to root dir of scripts i.e. $default_scripts_github"
+  echo "See INSTALL instructions at: https://github.com/aselvan/scripts?tab=readme-ov-file#setup"
+  exit 1
+fi
+# init logs
+log.init $my_logfile
+
 if [ "$os_name" != "Darwin" ] ; then
   log.error "error" "This script is for MacOS only!"
-  exit 1
+  exit 0
 fi
 
 # parse commandline options
@@ -132,8 +147,8 @@ while getopts $options opt ; do
       keychain_file_name="$OPTARG"
       keychain_file_path="${HOME}/Library/Keychains/${keychain_file_name}.keychain-db"
       ;;
-    a)
-      action="$OPTARG"
+    c)
+      command_name="$OPTARG"
       ;;
     w)
       website="$OPTARG"
@@ -144,7 +159,7 @@ while getopts $options opt ; do
     p)
       password="$OPTARG"
       ;;
-    c)
+    t)
       comment="$OPTARG"
       ;;
     v)
@@ -165,25 +180,18 @@ if [ ! -f $keychain_file_path ] ; then
   security create-keychain ${keychain_file_name}.keychain
 fi
 
-case $action in 
+case $command_name in 
   add)
-    if [ -z $password ] ; then
-      log.error "missing required argument 'password'! See usage"
-      usage
-    fi
-    log.stat "Adding password for website ($website) and username ($username) ..."
-    security add-generic-password -j "$comment" -s $website -a $username -w $password $keychain_file_name.keychain
+    do_add
     ;;
   delete)
-    log.stat "Deleting entry for website ($website) and username ($username) ..."
-    security delete-generic-password -s $website -a $username $keychain_file_name.keychain
+    do_delete
     ;;
   show)
-    log.stat "Password for website ($website) and username ($username) is below"
-    security find-generic-password -s $website -a $username -w $keychain_file_name.keychain    
+    do_show 
     ;;
   *)
-    log.error "Invalid action verb! See usage"
+    log.error "Invalid command! See usage for valid commands"
     usage
     ;;
 esac
