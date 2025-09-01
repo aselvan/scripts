cc47977848225aa2a9e64ae40df050a3fb6dd52e
Arul Selvan <aselvan@selvans.net>
2025-09-01 14:05:29 -0500

    Moved ipquality, projecthoneypot to ipabuse.sh
    Signed-off-by: Arul Selvan <aselvan@selvans.net>

--- security/ismalicious.sh
+++ security/ismalicious.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-# ismalicious.sh --- Query ismalicious.com API and/or Project Honeypot API
+# ismalicious.sh --- Query ismalicious.com API
 #
 # Check if an IP/Domain's reputation, vulnerabilities, geolocaion or whois info using the
 # ismalicious.com free API and/or projecthoneypot.org, and prints the JSON output.
@@ -14,40 +14,35 @@
 # Author:  Arul Selvan
 # Created: Jan 14, 2025
 #
+# See Also: ipabuse.sh isphishing.sh
+#
 # Version History:
-#   Jan 14,  2025 --- Orginal version
-#   Jan 17,  2025 --- Added additional check at ProjectHoneypot.org
-#   Jan 23,  2025 --- Option to select which service to use.
-#   Feb 5,   2025 --- Added ipqualityscore.com API
+#   Jan 14, 2025 --- Orginal version
+#   Jan 17, 2025 --- Added additional check at ProjectHoneypot.org
+#   Jan 23, 2025 --- Option to select which service to use.
+#   Feb 5,  2025 --- Added ipqualityscore.com API
+#   Sep 1,  2025 --- Moved ipquality, projecthoneypot to ipabuse.sh 
 #
 
 # version format YY.MM.DD
-version=25.01.23
+version=25.09.01
 my_name="`basename $0`"
 my_version="`basename $0` v$version"
-my_title="Query ismalicious.com API and/or Project Honeypot API"
+my_title="Query ismalicious.com API"
 my_dirname=`dirname $0`
 my_logfile="/tmp/$(echo $my_name|cut -d. -f1).log"
 default_scripts_github=$HOME/src/scripts.github
 scripts_github=${SCRIPTS_GITHUB:-$default_scripts_github}
 
 # commandline options
-options="c:n:s:k:K:A:vh?"
+options="c:n:k:vh?"
 
 supported_commands="reputation|vulnerabilities|geolocation|whois"
 ismalicious_api_url_base="https://ismalicious.com/api/check"
-ipqualityscore_api_url_base="https://www.ipqualityscore.com/api/json/url"
-projecthoneypart_dns_suffix="dnsbl.httpbl.org"
-
 ismalicious_api_key_file="$HOME/.ismalicious.com-apikey.txt"
-projecthoneypot_api_key_file="$HOME/.projecthoneypot.org-apikey.txt"
-ipqualityscore_api_key_file="$HOME/.ipqualityscore-apikey.txt"
 ismalicious_api_key=
-projecthoneypot_api_key=""
-ipqualityscore_api_key=""
 command_name="reputation"
 name=""
-service=0
 http_output="/tmp/$(echo $my_name|cut -d. -f1).txt"
 
 
@@ -58,100 +53,48 @@ $my_title
 Usage: $my_name [options]
   -c <command>  ---> command to run [Default: $command_name]. See supported commands below
   -n <name>     ---> Domain/IP name to check
-  -s <service>  ---> Service# to use [0-3] Accepted values are below [Default: $service]
-                     0=all 1=ismalicious 2=projecthoneypot 3=ipqualityscore 
   -k <apikey>   ---> ismalicious.com API key [Default: read from $ismalicious_api_key_file]
-  -K <apikey>   ---> ProjectHoneypot access key [Default: read from $projecthoneypot_api_key_file]
-  -A <apikey>   ---> IPqualityscore API key [Default: read from $ipqualityscore_api_key_file]
   -v            ---> enable verbose, otherwise just errors are printed
   -h            ---> print usage/help
 
 Supported commands: $supported_commands  
-example: $my_name -n 5.167.71.233
-example: $my_name -c whois -n qouv.fr
+example(s): 
+  $my_name -n 123.160.221.167
+  $my_name -c whois -n qouv.fr
  
 EOF
   exit 0
 }
 
+print_results() {
+  case $command_name in
+    reputation)
+      log.stat "\tScore:      `cat $http_output|jq -r '.confidenceScore'`" 
+      log.stat "\tMalicious:  `cat $http_output|jq -r '.reputation.malicious'`" 
+      log.stat "\tSuspicious: `cat $http_output|jq -r '.reputation.suspicious'`" 
+      log.stat "\t`cat $http_output|jq -r '.sources[] | "\(.category): \(.status)"'`" 
+      log.stat "\tOutput:  $http_output"
+    ;;
+    vulnerabilities|geolocation|whois)
+      cat $http_output|jq
+    ;;
+  esac
+}
+
 # check using ismalicious API
 check_ismalicious() {
   log.stat "Checking $command_name of $name using ismalicious API ..."
+
   http_status=$(curl -s -o $http_output -w "%{http_code}" -H "X-API-KEY: $ismalicious_api_key" ${ismalicious_api_url_base}/${command_name}?query=$name)
+
   log.debug "HTTP status: $http_status"
   if [ "$http_status" -eq 200 ] ; then
-    check_installed jq noexit
-    if [ $? -eq 0 ] ; then
-      cat $http_output | jq
-    else
-      cat $http_output
-    fi
+    print_results
   elif [ "$http_status" -eq 429 ] ; then
     log.warn  "\tAPI returned: 'too many requests', retry again after few seconds."
   else
     log.error "\tAPI call failed! HTTP status = $http_status"
   fi
-}
-
-# decode the response
-# See documentation at https://www.projecthoneypot.org/httpbl_api.php
-#
-# example decode of a return response: 127.1.55.1 
-#   127 - always 127. If it is not 127, there was an error
-#   1   - number of days last activity (0-255). Lower is most recent
-#   55  - threat score (0-255). Higher is bad, 0 is no threat.
-#   1   - type (0=searchengine; 1=suspicious, 2=harvester, 4=comment_spammer, rest are future use)
-decode_projecthoneypot_response() {
-  local resp=$1
-  IFS='.' read -r -a resp_array <<< "$resp"
-  if [ ${resp_array[0]} -ne 127 ] ; then
-    log.error "\tERROR: response is not ${resp_array[2]} is not expected 127"
-    return
-  fi
-  log.stat "\tMalicious:    YES [seen as recently as of last ${resp_array[1]} day(s)]." $red
-  log.stat "\tThreat score: ${resp_array[2]}/255. [Note: score of 0 is clean]" $red
-  log.stat "\tThreat type:  ${resp_array[3]} [note: 0=searchengine; 1=suspicious, 2=harvester, 4=comment_spammer]" $red
-}
-
-check_projecthoneypot() {
-  # if name is not IP just ignore
-  log.debug "Validating $name for IP address ..."
-  validate_ip $name
-  if [ $? -ne 0 ] ; then
-    return
-  fi
-
-  log.stat "Checking $command_name of $name using ProjectHoneypot API ..." 
-  # reverse IP to make DNS query with projecthonepot 
-  rev_ip=$(reverse_ip $name)
-  log.debug "Reversed IP: $rev_ip"
-
-  dns_resp=`dig +short ${projecthoneypot_api_key}.$rev_ip.${projecthoneypart_dns_suffix}`
-  log.debug "Project Honeypot response: $dns_resp"
-  if [ "$dns_resp" != "" ] ; then
-    decode_projecthoneypot_response $dns_resp
-  else
-    log.warn "\tEmpty response ProjectHoneypot API, likely no entry for this IP."
-  fi
-}
-
-check_ipqualityscore() {
-  log.stat "Checking $command_name of $name using ipqualityscore.com API ..."
-  http_status=$(curl -s -o $http_output -w "%{http_code}" ${ipqualityscore_api_url_base}/${ipqualityscore_api_key}/${name})
-  log.debug "HTTP status: $http_status"
-  if [ "$http_status" -eq 200 ] ; then
-    check_installed jq noexit
-    if [ $? -eq 0 ] ; then
-      cat $http_output | jq
-    else
-      cat $http_output
-    fi
-  elif [ "$http_status" -eq 429 ] ; then
-    log.warn  "\tAPI returned: 'too many requests', retry again after few seconds."
-  else
-    log.error "\tAPI call failed! HTTP status = $http_status"
-  fi
-
 }
 
 
@@ -170,15 +113,12 @@ fi
 # init logs
 log.init $my_logfile
 
+# require jq
+check_installed jq
+
 # read API and access keys for ismalicious.com and projecthoneypot.org
 if [ -f $ismalicious_api_key_file ] ; then
   ismalicious_api_key=`cat $ismalicious_api_key_file`
-fi
-if [ -f $projecthoneypot_api_key_file ] ; then
-  projecthoneypot_api_key=`cat $projecthoneypot_api_key_file`
-fi
-if [ -f $ipqualityscore_api_key_file ] ; then
-  ipqualityscore_api_key=`cat $ipqualityscore_api_key_file`
 fi
 
 # parse commandline options
@@ -190,21 +130,8 @@ while getopts $options opt ; do
     n)
       name="$OPTARG"
       ;;
-    s)
-      service="$OPTARG"
-      if [[ ! "$service" =~ ^[0-3]$ ]] ; then
-        log.error "Invalid service! It must be between 0-3, see usage below ..."
-        usage
-      fi
-      ;;
     k)
       ismalicious_api_key="$OPTARG"
-      ;;
-    K)
-      projecthoneypot_api_key="$OPTARG"
-      ;;
-    A)
-      ipqualityscore_api_key="$OPTARG"
       ;;
     v)
       verbose=1
@@ -221,55 +148,15 @@ if [[ "|$supported_commands|" != *"|$command_name|"* ]] ; then
   usage
 fi
 
-
 if [ -z "$name" ] ; then
   log.error "Missing name to query, see usage below"
   usage
 fi
 
-case $service in
-  0)
-    if [ -z "$ismalicious_api_key" ] && [ -z $projecthoneypot_api_key ] && [ -z $ipqualityscore_api_key] ; then
-      log.error "Need API keys for service call, see usage below"
-      usage
-    fi
-    # check ismalicious.com
-    check_ismalicious
-    # if the command is "reputation" check ProjectHoneyPot as well
-    if [ "$command_name" == "reputation" ] ; then
-      check_projecthoneypot
-    fi
-    # check ipqualityscore
-    check_ipqualityscore
-    ;;
-  1)
-    if [ -z "$ismalicious_api_key" ] ; then
-      log.error "Need API keys for ismalicious.com, see usage below"
-      usage
-    fi
-    # check ismalicious.com
-    check_ismalicious
-    ;;
-  2)
-    if [ -z "$projecthoneypot_api_key" ] ; then
-      log.error "Need access keys for ProjectHoneypot, see usage below"
-      usage
-    fi
-    # if the command is "reputation" check ProjectHoneyPot as well
-    if [ "$command_name" == "reputation" ] ; then
-      check_projecthoneypot
-    fi
-    ;;
-  3)
-    if [ -z "$ipqualityscore_api_key" ] ; then
-      log.error "Need access keys for IPqualityscore.com, see usage below"
-      usage
-    fi
-    check_ipqualityscore
-    ;;
-  *)
-    log.error "Invalid service: $service"
-    usage
-    ;;
-esac
+if [ -z "$ismalicious_api_key" ]  ; then
+  log.error "Need API keys for ismalicious service call, see usage below"
+  usage
+fi
+
+check_ismalicious
 
